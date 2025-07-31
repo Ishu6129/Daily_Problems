6bc0a2cbce7ba06d496c7a7e6b88754d6955bee8
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-07-31 19:34:03 +0530

    Create 795-KThSymbolInGrammar

---
+++ 795-KThSymbolInGrammar/795-KThSymbolInGrammar.java
@@ -0,0 +1,19 @@
+// Last updated: 31/07/2025, 19:33:22
+class Solution {
+    public int kthGrammar(int n, int k) {
+        int cur=0;
+        int left=1;
+        int right=(int)Math.pow(2,n-1);
+        for(int i=0;i<n-1;i++){
+            int mid=(left+right)/2;
+            if(k<=mid){
+                right=mid;
+            }
+            else{
+                left=mid+1;
+                cur = (cur==1?0:1);
+            }
+        }
+        return cur; 
+    }
+}
