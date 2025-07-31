85831acb12666bdfb0979636873ee4662b3234a8
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-01 00:36:32 +0530

    Create 3515-FindIfDigitGameCanBeWon

---
+++ 3515-FindIfDigitGameCanBeWon/3515-FindIfDigitGameCanBeWon.java
@@ -0,0 +1,14 @@
+// Last updated: 01/08/2025, 00:35:52
+class Solution {
+    public boolean canAliceWin(int[] nums) {
+        int s_sum = 0;
+        int d_sum = 0;
+        
+        for(int num : nums) {
+            if(num < 10) s_sum += num;
+            else d_sum += num;
+        }
+        
+        return s_sum != d_sum;
+    }
+}
