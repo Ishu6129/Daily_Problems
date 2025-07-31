9c17c6293ed8c7fd91acde0f6a9086751233e6ff
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-07-31 19:42:51 +0530

    Create 20-ValidParentheses

---
+++ 20-ValidParentheses/20-ValidParentheses.java
@@ -0,0 +1,29 @@
+// Last updated: 31/07/2025, 19:42:11
+class Solution {
+    public boolean isValid(String s) {
+        if(s.length()<2){
+            return false;
+        }
+        Stack<Character> st=new Stack<>();
+        for(int i=0;i<s.length();i++){
+            char ch=s.charAt(i);
+            if(ch=='(' || ch=='[' || ch=='{' ){
+                st.push(ch);
+             }
+            else{
+                if(!st.isEmpty()){
+                    if(ch==')' && st.peek()=='(' || ch==']' && st.peek()=='[' || ch=='}' && st.peek()=='{'){
+                        st.pop();
+                    }
+                    else{
+                        return false;
+                    }
+                }
+                else{
+                    return false;
+                }
+        }
+     } 
+     return st.isEmpty();  
+    }
+}
