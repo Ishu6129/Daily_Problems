67043666632824b5dde53cb8ace2609110d89345
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-01 00:36:19 +0530

    File updated at 01/08/2025, 00:35:39

--- 3232-FindifDigitGameCanBeWon/3232-FindifDigitGameCanBeWon.java
+++ 3232-FindifDigitGameCanBeWon/3232-FindifDigitGameCanBeWon.java
@@ -1,22 +1,14 @@
-// Last updated: 01/08/2025, 00:34:27
+// Last updated: 01/08/2025, 00:35:39
 class Solution {
     public boolean canAliceWin(int[] nums) {
-        int s_sum=0;
-        int d_sum=0;
-        int i=0;
-        int j=nums.length-1;
-        while(i<j){
-            if(nums[i]<10) s_sum+=nums[i];
-            else d_sum+=nums[i];
-            if(nums[j]<10) s_sum+=nums[j];
-            else d_sum+=nums[j];
-            i++;
-            j--;
+        int s_sum = 0;
+        int d_sum = 0;
+        
+        for(int num : nums) {
+            if(num < 10) s_sum += num;
+            else d_sum += num;
         }
-        if(nums.length%2!=0){
-            if(nums[nums.length/2]<10) s_sum+=nums[nums.length/2];
-            else d_sum+=nums[nums.length/2];
-        }
-        return d_sum>s_sum ||d_sum<s_sum;
+        
+        return s_sum != d_sum;
     }
 }
