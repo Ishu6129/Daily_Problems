3b4efcbf67ddc0ab6760b31d6fa14cb0384db689
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-07-31 19:33:46 +0530

    Create 954-MaximumSumCircularSubarray

---
+++ 954-MaximumSumCircularSubarray/954-MaximumSumCircularSubarray.java
@@ -0,0 +1,23 @@
+// Last updated: 31/07/2025, 19:33:05
+class Solution {
+    public int maxSubarraySumCircular(int[] arr) {
+      int totalSum=0;
+        int curr_max=0;
+        int curr_min=0;
+        int max_sum=arr[0];
+        int min_sum=arr[0];
+        for(int i:arr){
+            curr_max=Math.max(i,curr_max+i);
+            max_sum=Math.max(max_sum,curr_max);
+            curr_min=Math.min(i,curr_min+i);
+            min_sum=Math.min(min_sum,curr_min);
+            totalSum+=i;
+        }
+        int kadSum=max_sum;
+        int cirSum=totalSum-min_sum;
+        if (kadSum < 0) {
+            return kadSum;
+        }
+        return Math.max(kadSum,cirSum);
+    }
+}
