d0bc3b3bc9fd5313f5896b5163615e794926fea1
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-07 18:27:01 +0530

    Create 125-ValidPalindrome

---
+++ 125-ValidPalindrome/125-ValidPalindrome.java
@@ -0,0 +1,8 @@
+// Last updated: 07/08/2025, 18:23:18
+class Solution {
+    public boolean isPalindrome(String s) {
+        String str = s.replaceAll("[^a-zA-Z0-9]", "").toLowerCase();
+        String reversed = new StringBuilder(str).reverse().toString();
+        return str.equals(reversed);
+    }
+}
