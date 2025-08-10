21b23f0fc9ef62e2c81a73686c35b0e960dc1af1
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-10 22:10:00 +0530

    Create 59-SpiralMatrixII

---
+++ 59-SpiralMatrixII/59-SpiralMatrixII.java
@@ -0,0 +1,40 @@
+// Last updated: 10/08/2025, 22:06:15
+class Solution {
+    public int[][] generateMatrix(int n) {
+        return clockWise(n);
+    }
+    public int[][] clockWise(int n){
+        int[][] arr=new int[n][n];
+        int minc=0,minr=0,maxc=arr[0].length-1,maxr= arr.length-1;
+        int t=arr.length*arr[0].length;
+        int c=0;
+        int elem=1;
+        while(c<t) {
+            for (int i = minc; i <= maxc && c<t; i++) {
+                arr[minr][i]=elem;
+                elem++;
+                c++;
+            }
+            minr++;
+            for (int i = minr; i <= maxr && c<t; i++) {
+                arr[i][maxc]=elem;
+                elem++;
+                c++;
+            }
+            maxc--;
+            for (int i = maxc; i >= minc && c<t; i--) {
+                arr[maxr][i]=elem;
+                elem++;
+                c++;
+            }
+            maxr--;
+            for (int i = maxr; i >= minr && c<t; i--) {
+                arr[i][minc]=elem;
+                elem++;
+                c++;
+            }
+            minc++;
+        }
+        return arr;
+    }
+}
