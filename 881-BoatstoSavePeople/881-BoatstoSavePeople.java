7793595a2d75bfed71dfdf719fc365d813121743
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-17 21:55:24 +0530

    File updated at 17/08/2025, 21:51:32

--- 881-BoatstoSavePeople/881-BoatstoSavePeople.java
+++ 881-BoatstoSavePeople/881-BoatstoSavePeople.java
@@ -1,4 +1,4 @@
-// Last updated: 17/08/2025, 21:50:30
+// Last updated: 17/08/2025, 21:51:32
 class Solution {
     public int numRescueBoats(int[] people, int limit) {
         Arrays.sort(people);
@@ -7,14 +7,10 @@ class Solution {
         int ans=0;
         while(i<j){
             if(people[i]+people[j]<=limit){
-                ans++;
                 i++;
-                j--;
             }
-            else{
-                ans++;
-                j--;
-            }
+            j--;
+            ans++;
         }
         if(i==j) return ++ans;
         return ans;
