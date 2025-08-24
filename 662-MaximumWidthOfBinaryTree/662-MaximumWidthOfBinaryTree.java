ba2ecac5bf75debec89b9a54746054517409405a
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-24 22:52:17 +0530

    Create 662-MaximumWidthOfBinaryTree

---
+++ 662-MaximumWidthOfBinaryTree/662-MaximumWidthOfBinaryTree.java
@@ -0,0 +1,52 @@
+// Last updated: 24/08/2025, 22:48:17
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+// width=end_id-start_idx+1
+
+class Solution {
+    class Pair{
+        TreeNode node;
+        int idx;
+        public Pair(TreeNode node,int idx){
+            this.node=node;
+            this.idx=idx;
+        }
+    }
+    public int widthOfBinaryTree(TreeNode root) {
+        Queue<Pair> q = new LinkedList<>();
+        int width=0;
+        if (root != null) {
+            q.add(new Pair(root,0));
+        }
+        while (!q.isEmpty()) {
+            Pair start=q.peek();
+            int s=q.size();
+            Pair end=null;
+            for(int i=0;i<s;i++){
+                end=q.poll();
+                if(end.node.left!=null){
+                    q.add(new Pair(end.node.left,2*end.idx+1));
+                }
+                if(end.node.right!=null){
+                    q.add(new Pair(end.node.right,2*end.idx+2));
+                }
+            }
+            width=Math.max(width,end.idx-start.idx+1);
+        }
+        return width;
+    }
+}
