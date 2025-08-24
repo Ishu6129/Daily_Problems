2abbb1c71d467ad157db0779c92eb3a7e11fc41a
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-24 22:53:39 +0530

    Create 74-SearchA2dMatrix

---
+++ 74-SearchA2dMatrix/74-SearchA2dMatrix.java
@@ -0,0 +1,23 @@
+// Last updated: 24/08/2025, 22:49:40
+class Solution {
+    public boolean searchMatrix(int[][] matrix, int target) {
+        return StairCaseSearch(matrix,target);
+    }
+    public static Boolean StairCaseSearch(int[][] arr,int value){
+        int row=0;
+        int col= arr[0].length-1;
+        while(row<arr.length && col>=0){
+            if(arr[row][col]==value){
+                return true;
+            }
+            else if (arr[row][col]>value) {
+                col--;
+            }
+            else{
+                row++;
+            }
+        }
+        return false;
+
+    }
+}
