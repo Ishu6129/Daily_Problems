746fad85a297ee90e4f756ac48062f2f9aa055d7
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-08-24 22:53:03 +0530

    Create 230-KthSmallestElementInABst

---
+++ 230-KthSmallestElementInABst/230-KthSmallestElementInABst.java
@@ -0,0 +1,37 @@
+// Last updated: 24/08/2025, 22:49:04
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int n=0;
+    int ans=-1;
+    public int kthSmallest(TreeNode root, int k) {
+        find(root,k);
+        return ans;
+    }
+    public void find(TreeNode root,int k){
+        if(root==null){
+            return;
+        }
+        find(root.left,k);
+        n++;
+        if(n==k){
+            ans=root.val;
+            return;
+        }
+        find(root.right,k);
+        return ;
+    }
+}
