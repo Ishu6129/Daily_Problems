6a99857e864ec8afd41fed90d360eef1f013dc18
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-09-04 14:13:25 +0530

    File updated at 04/09/2025, 14:13:08

--- 3516-FindClosestPerson/3516-FindClosestPerson.java
+++ 3516-FindClosestPerson/3516-FindClosestPerson.java
@@ -1,8 +1,43 @@
-// Last updated: 04/09/2025, 14:12:52
+// Last updated: 04/09/2025, 14:13:08
 class Solution {
-    public int findClosest(int x, int y, int z) {
-        if(Math.abs(x-z)==Math.abs(y-z)) return 0;
-        if(Math.abs(x-z)<Math.abs(y-z)) return 1;
-        return 2;
+    public int findClosest(int x, int y, int z) 
+    {
+        int a = Math.abs(z-x);
+        int b=Math.abs(z-y);
+        if(a==b)
+        {
+            return 0;
+        }
+          if(a<b) return 1;
+          else  return 2;     
     }
+    // public int numx(int x,int z)
+    // {
+    //     if(x<z) return inc(x,z);
+    //     else   return dec(x,z);  
+        
+    // }
+    
+    // public int inc(int a,int b)
+    // {
+    //     int s=0;
+    //     for(int i=a;i<b;i++)
+    //     {
+    //         if(i==b)
+    //         {
+    //         break;
+    //         }
+    //         s++;
+    //     }
+    //     return s;
+    // }
+    // public int dec(int a,int b)
+    // {
+    //     int s=0;
+    //     for(int i=a;i>b;i--)
+    //     {
+    //         s++;
+    //     }
+    //     return s;
+    // }
 }
