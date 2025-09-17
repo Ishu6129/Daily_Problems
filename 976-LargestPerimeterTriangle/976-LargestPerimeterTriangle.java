b4b91e5aa70663125cd1be68dae0ba05ae7eb48b
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-09-17 20:14:55 +0530

    Create 976-LargestPerimeterTriangle

---
+++ 976-LargestPerimeterTriangle/976-LargestPerimeterTriangle.java
@@ -0,0 +1,10 @@
+// Last updated: 17/09/2025, 20:14:09
+class Solution {
+    public int largestPerimeter(int[] nums) {
+        Arrays.sort(nums);
+        for(int i=nums.length-1;i>1;i--){
+            if(nums[i-2]+nums[i-1]>nums[i]) return nums[i-2]+nums[i-1]+nums[i]; 
+        }
+        return 0;
+    }
+}
