44c3f3edfd1c0f846d6217b764fc10780d1d447d
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-10-12 00:54:09 +0530

    Create 1264-MaximumNumberOfWordsYouCanType

---
+++ 1264-MaximumNumberOfWordsYouCanType/1264-MaximumNumberOfWordsYouCanType.java
@@ -0,0 +1,22 @@
+// Last updated: 12/10/2025, 00:54:18
+class Solution {
+    public int canBeTypedWords(String text, String brokenLetters) {
+        Map<Character, Boolean> map = new HashMap<>();
+        for (char ch : brokenLetters.toCharArray()) {
+            map.put(ch, true);
+        }
+        String[] arr = text.split(" ");
+        int c = 0;
+        for (int i = 0; i < arr.length; i++) {
+            boolean ctp = true;
+            for (char ch : arr[i].toCharArray()) {
+                if (map.containsKey(ch)) {
+                    ctp = false;
+                    break;
+                }
+            }
+            if (ctp) c++;
+        }
+        return c;
+    }
+}
