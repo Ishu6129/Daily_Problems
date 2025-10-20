06d12b1c7506bb94be3fc99aa59d1ed105475f46
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-10-20 12:51:20 +0530

    File updated at 20/10/2025, 12:51:22

--- 2011-FinalValueofVariableAfterPerformingOperations/2011-FinalValueofVariableAfterPerformingOperations.java
+++ 2011-FinalValueofVariableAfterPerformingOperations/2011-FinalValueofVariableAfterPerformingOperations.java
@@ -1,13 +1,13 @@
-// Last updated: 20/10/2025, 12:50:32
+// Last updated: 20/10/2025, 12:51:22
 class Solution {
     public int finalValueAfterOperations(String[] operations) {
-        int ans=0;
-        for(String s:operations){
-            if(s.charAt(0)=='+') ++ans;
-            else if(s.charAt(1)=='+') ans++;
-            else if(s.charAt(0)=='-') --ans;
-            else ans--;
+        int x = 0;
+
+        for(String s : operations){
+            if(s.equals("++X") || s.equals("X++")) x++;
+            else x--;
         }
-        return ans;
+
+        return x;
     }
 }
