de41cfef6f42cdbdd8a8b581fe5b3fc8ae519c3a
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-11-07 22:27:51 +0530

    File updated at 07/11/2025, 22:25:47

--- 89-GrayCode/89-GrayCode.java
+++ 89-GrayCode/89-GrayCode.java
@@ -1,4 +1,4 @@
-// Last updated: 07/11/2025, 22:21:16
+// Last updated: 07/11/2025, 22:25:47
 class Solution {
     public List<Integer> grayCode(int n) {
         if(n==1){
@@ -12,9 +12,10 @@ class Solution {
        for(int i:pre){
             ans.add(i);
        }
-       int add = 1 << (n - 1);
        for (int i = pre.size() - 1; i >= 0; i--) {
-            ans.add(pre.get(i) | add);
+            String bin = Integer.toBinaryString(pre.get(i));
+            bin = String.format("%" + (n-1) + "s", bin).replace(' ', '0');
+            ans.add(Integer.valueOf("1" + bin, 2));
         }
        return ans;
     }
