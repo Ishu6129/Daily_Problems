81589bcac3eccc8efb909cf4f3cea0d8e4521e10
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-11-19 12:54:23 +0530

    Create 2154-KeepMultiplyingFoundValuesbyTwo

---
+++ 2154-KeepMultiplyingFoundValuesbyTwo/2154-KeepMultiplyingFoundValuesbyTwo.java
@@ -0,0 +1,13 @@
+// Last updated: 19/11/2025, 12:52:08
+class Solution {
+    public int findFinalValue(int[] nums, int original) {
+        Set<Integer> s=new HashSet<>();
+        for(int i:nums){
+            s.add(i);
+        }
+        while(s.contains(original)){
+            original*=2;
+        }
+        return original;
+    }
+}
