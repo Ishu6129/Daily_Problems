f2b0c416679bdae4089a024ca8fb1d2e7b82b45b
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-11-23 20:29:51 +0530

    Create 2119-ANumberAfteraDoubleReversal

---
+++ 2119-ANumberAfteraDoubleReversal/2119-ANumberAfteraDoubleReversal.java
@@ -0,0 +1,7 @@
+// Last updated: 23/11/2025, 20:27:31
+class Solution {
+    public boolean isSameAfterReversals(int num) {
+        if(num==0) return true;
+        return num%10==0?false:true;
+    }
+}
