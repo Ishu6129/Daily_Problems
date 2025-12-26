5536d2e3bb15a3509f18a81606fe05419296baf1
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-12-26 12:28:02 +0530

    File updated at 26/12/2025, 12:25:07

--- 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
+++ 2187-MinimumTimetoCompleteTrips/2187-MinimumTimetoCompleteTrips.java
@@ -1,28 +1,28 @@
-// Last updated: 26/09/2025, 22:11:17
-class Solution {
-    public long minimumTime(int[] time, int totalTrips) {
-        Arrays.sort(time);
-        long left = 1;
-        long right = (long) time[time.length - 1] * totalTrips;
-        long ans=right;
-        while(left<=right){
-            long mid=left+(right-left)/2;
-             if(check(time,mid,totalTrips)){
-                ans=mid;
-                right=mid-1;
-             }
-             else{
-                left=mid+1;
-             }
-        }
-        return ans;
-    }
-    public boolean check(int[] time,long mid,long target){
-        long c=0;
-        for(int i=0;i<time.length;i++){
-            c=c+(mid/time[i]);
-        }
-        return c>=target?true:false;
-    }
-
-}
+// Last updated: 26/12/2025, 12:25:07
+1class Solution {
+2    public long minimumTime(int[] time, int totalTrips) {
+3        Arrays.sort(time);
+4        long left = 1;
+5        long right=(long)time[time.length-1]*totalTrips;
+6        long ans=right;
+7        while(left<=right){
+8            long mid=left+(right-left)/2;
+9             if(check(time,mid,totalTrips)){
+10                ans=mid;
+11                right=mid-1;
+12             }
+13             else{
+14                left=mid+1;
+15             }
+16        }
+17        return ans;
+18    }
+19    public boolean check(int[] time,long mid,long target){
+20        long c=0;
+21        for(int i=0;i<time.length;i++){
+22            c=c+(mid/time[i]);
+23        }
+24        return c>=target?true:false;
+25    }
+26
+27}
