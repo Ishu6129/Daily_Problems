7169ac50140ee3fe3d5e05ccfae1212ed79a205b
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-12-31 12:21:45 +0530

    File updated at 31/12/2025, 12:21:41

--- 14-LongestCommonPrefix/14-LongestCommonPrefix.java
+++ 14-LongestCommonPrefix/14-LongestCommonPrefix.java
@@ -1,19 +1,54 @@
-// Last updated: 05/08/2025, 00:28:20
-class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if(strs.length == 0) return "";
-        if(strs.length == 1) return strs[0];
-        String ans = strs[0];
-        for(int i = 1; i < strs.length; i++) {
-            for(int j = 0; j < Math.min(ans.length(), strs[i].length()); j++) {
-                if(ans.charAt(j) != strs[i].charAt(j)) {
-                    ans = ans.substring(0, j);
-                    break;
-                }
-            }
-            ans = ans.substring(0, Math.min(ans.length(), strs[i].length()));
-            if(ans.equals("")) return "";
-        }
-        return ans;
-    }
-}
+// Last updated: 31/12/2025, 12:21:41
+1class Solution {
+2    class TrieNode {
+3        TrieNode[] children;
+4        boolean isEndOfWord;
+5
+6        TrieNode() {
+7            children = new TrieNode[26];
+8            isEndOfWord = false;
+9        }
+10    }
+11    TrieNode root = new TrieNode();
+12    public void insert(String word) {
+13        TrieNode current = root;
+14
+15        for (int i = 0; i < word.length(); i++) {
+16            int index = word.charAt(i) - 'a';
+17
+18            if (current.children[index] == null) {
+19                current.children[index] = new TrieNode();
+20            }
+21            current = current.children[index];
+22        }
+23        current.isEndOfWord = true;
+24    }
+25    public String check(String word) {
+26        TrieNode current = root;
+27        StringBuilder sb=new StringBuilder();
+28        for (int i=0;i<word.length(); i++) {
+29            int index=word.charAt(i) - 'a';
+30            if (current.children[index] == null) {
+31                return sb.toString();
+32            }
+33            else{
+34                sb.append(word.charAt(i));
+35            }
+36            current=current.children[index];
+37        }
+38        return sb.toString();
+39    }
+40     public String longestCommonPrefix(String[] strs) {
+41        if(strs.length==0) return "";
+42        insert(strs[0]);
+43        String ans=strs[0];
+44        for(int i=1;i<strs.length;i++){
+45            String match=check(strs[i]);
+46            if(match.length()<ans.length()){
+47                ans=match;
+48            }
+49        }
+50        return ans;
+51
+52    }
+53}
