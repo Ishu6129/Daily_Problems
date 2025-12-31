5c20cee0a912814c1d39f94528d326c32db62460
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2025-12-31 16:01:25 +0530

    File updated at 31/12/2025, 16:01:21

--- 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
+++ 1971-FindifPathExistsinGraph/1971-FindifPathExistsinGraph.java
@@ -1,28 +1,28 @@
-// Last updated: 31/12/2025, 15:56:29
+// Last updated: 31/12/2025, 16:01:21
 1class Solution {
 2    public boolean validPath(int n, int[][] edges, int source, int destination) {
-3        HashMap<Integer,List<Integer>> map=new HashMap<>();
-4        for(int i=0;i<n;i++){
-5            map.put(i,new ArrayList<>());
-6        }
-7        for(int[] e:edges){
-8            map.get(e[0]).add(e[1]);
-9            map.get(e[1]).add(e[0]);
-10        }
-11        int[] ans=new int[n];
-12        Arrays.fill(ans,-1);
+3        if(source==destination) return true;
+4        HashMap<Integer,List<Integer>> map=new HashMap<>();
+5        for(int i=0;i<n;i++){
+6            map.put(i,new ArrayList<>());
+7        }
+8        for(int[] e:edges){
+9            map.get(e[0]).add(e[1]);
+10            map.get(e[1]).add(e[0]);
+11        }
+12        Set<Integer> set=new HashSet<>();
 13        Queue<Integer> q=new LinkedList<>();
 14        q.add(source);
-15        ans[source]=0;
-16        while(!q.isEmpty()){
-17            int r=q.poll();
-18            for(int nbrs:map.get(r)){
-19                if(ans[nbrs]==-1){
-20                    ans[nbrs]=ans[r]+1;
+15        while(!q.isEmpty()){
+16            int r=q.poll();
+17            for(int nbrs:map.get(r)){
+18                if(!set.contains(nbrs)){
+19                    if(nbrs==destination) return true;
+20                    set.add(nbrs);
 21                    q.add(nbrs);
 22                }
 23            }
 24        }
-25        return (ans[destination]==-1)? false:true;
+25        return false;
 26    }
 27}
