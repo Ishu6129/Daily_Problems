e79e05f50cb1ab786fdab8dec9555b54af3e86ff
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-01 23:09:12 +0530

    File updated at 01/01/2026, 23:09:08

--- 66-PlusOne/66-PlusOne.java
+++ 66-PlusOne/66-PlusOne.java
@@ -1,25 +1,23 @@
-// Last updated: 31/07/2025, 19:40:52
-class Solution {
-    public int[] plusOne(int[] digits) {
-        int last=digits[digits.length-1];
-        int c=(last+1)/10;
-        int s=(last+1)%10;
-        int[] arr=new int[digits.length];
-        arr[arr.length-1]=s;
-        for (int i = digits.length - 2; i > -1; i--) {
-            s = digits[i] + c;
-            arr[i] = s % 10;
-            c = s / 10;
-        }
-        if(c!=0){
-            int[] result = new int[digits.length + 1];
-            result[0] = c;
-            for (int i = 0; i < arr.length; i++) {
-                result[i + 1] = arr[i];
-            }
-            return (result);
-        }
-        return (arr);
-        }
-
-    }
+// Last updated: 01/01/2026, 23:09:08
+1class Solution {
+2    public int[] plusOne(int[] digits) {
+3        int n=digits.length;
+4        int d=(digits[n-1]+1)%10;
+5        int c=(digits[n-1]+1)/10;
+6        digits[n-1]=d;
+7        for(int i=n-2;i>=0;i--){
+8            d=(digits[i]+c)%10;
+9            c=(digits[i]+c)/10;
+10            digits[i]=d;
+11        }
+12        if(c==1){
+13            int[] ans=new int[n+1]; 
+14            for(int i=1;i<n+1;i++){
+15                ans[i]=digits[i-1];
+16            }
+17            ans[0]=1;
+18            return ans;
+19        }
+20        return digits;
+21    }
+22}
