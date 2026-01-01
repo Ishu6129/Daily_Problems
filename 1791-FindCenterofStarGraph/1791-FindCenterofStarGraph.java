6084d7ff1732df0dd0fd3818c6a81e4a3c75a47b
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-01 10:50:30 +0530

    File updated at 01/01/2026, 10:50:27

--- 1791-FindCenterofStarGraph/1791-FindCenterofStarGraph.java
+++ 1791-FindCenterofStarGraph/1791-FindCenterofStarGraph.java
@@ -1,22 +1,9 @@
-// Last updated: 01/01/2026, 10:46:58
+// Last updated: 01/01/2026, 10:50:27
 1class Solution {
 2    public int findCenter(int[][] edges) {
-3        HashMap<Integer,ArrayList<Integer>> map=new HashMap<>();
-4        Set<Integer> set=new HashSet<>();
-5        for(int[] i:edges){
-6            set.add(i[0]);
-7            set.add(i[1]);
-8        }
-9        for(int i=1;i<=set.size();i++){
-10            map.put(i,new ArrayList<>());
-11        }
-12        for(int i[]:edges){
-13            map.get(i[0]).add(i[1]);
-14            map.get(i[1]).add(i[0]);
-15        }
-16        for(int key:map.keySet()){
-17            if(map.get(key).size()==(set.size()-1)) return key;
-18        }
-19        return -1;
-20    }
-21}
+3        int[] arr=new int[edges.length+1];
+4        for(int[] i:edges){ arr[i[0]-1]++;arr[i[1]-1]++;}
+5        for(int i=0;i<arr.length;i++){if(arr[i]==edges.length) return i+1;}
+6        return -1;
+7    }
+8}
