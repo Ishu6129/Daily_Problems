8918dcf0db038ddd7499bf0faab9fa41e422f30f
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-01 12:09:49 +0530

    File updated at 01/01/2026, 12:09:46

--- 134-GasStation/134-GasStation.java
+++ 134-GasStation/134-GasStation.java
@@ -1,22 +1,22 @@
-// Last updated: 31/07/2025, 19:34:45
-class Solution {
-    public int canCompleteCircuit(int[] gas, int[] cost) {
-        int total=0;
-        for(int i=0;i<cost.length;i++){
-            total+=gas[i]-cost[i];
-        }
-        if (total<0){
-            return -1;
-        }
-        int idx=0;
-        int curr=0;
-        for(int i=0;i<cost.length;i++){
-            curr+=gas[i]-cost[i];
-            if (curr<0){
-                curr=0;
-                idx=i+1;
-            }
-        }
-        return idx;  
-    }
-}
+// Last updated: 01/01/2026, 12:09:46
+1class Solution {
+2    public int canCompleteCircuit(int[] gas, int[] cost) {
+3        int total=0;
+4        for(int i=0;i<cost.length;i++){
+5            total+=gas[i]-cost[i];
+6        }
+7        if (total<0){
+8            return -1;
+9        }
+10        int idx=0;
+11        int curr=0;
+12        for(int i=0;i<cost.length;i++){
+13            curr+=gas[i]-cost[i];
+14            if (curr<0){
+15                curr=0;
+16                idx=i+1;
+17            }
+18        }
+19        return idx;  
+20    }
+21}
