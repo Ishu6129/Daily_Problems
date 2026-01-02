62fbd750e0e7fcd9551e95e9807f85688cad7d45
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-02 23:30:18 +0530

    File updated at 02/01/2026, 23:30:13

--- 3112-MinimumTimetoVisitDisappearingNodes/3112-MinimumTimetoVisitDisappearingNodes.java
+++ 3112-MinimumTimetoVisitDisappearingNodes/3112-MinimumTimetoVisitDisappearingNodes.java
@@ -1,4 +1,4 @@
-// Last updated: 02/01/2026, 23:28:45
+// Last updated: 02/01/2026, 23:30:13
 1class Solution {
 2     class Pair{
 3        int vtx;
@@ -19,32 +19,30 @@
 18        }
 19        int[] ans=new int[n];
 20        Arrays.fill(ans,Integer.MAX_VALUE);
-21        
-22        PriorityQueue<Pair> q=new PriorityQueue<>((a,b)->a.cost-b.cost);
-23        q.add(new Pair(0,0));
-24        ans[0]=0;
-25        int time=0;
-26        while(!q.isEmpty()){
-27            Pair r=q.poll();
-28            int vtx=r.vtx;
-29            int cost=r.cost;
-30            if (cost > ans[vtx]) continue;
-31            if (cost >= disappear[vtx]) continue;
-32            for (Map.Entry<Integer, Integer> nbr : map.get(vtx).entrySet()) {
-33                int nbrVtx = nbr.getKey();
-34                int wt = nbr.getValue();
-35                int newTime = cost + wt;
-36                if (newTime < ans[nbrVtx] && newTime < disappear[nbrVtx]) {
-37                    ans[nbrVtx] = newTime;
-38                    q.add(new Pair(nbrVtx, newTime));
-39                }
-40            }
-41        }
-42        for (int i=0;i<n;i++) {
-43            if (ans[i]==Integer.MAX_VALUE) {
-44                ans[i]=-1;
-45            }
-46        }
-47        return ans;
-48    }
-49}
+21        PriorityQueue<Pair> q=new PriorityQueue<>((a,b)->a.cost-b.cost);
+22        q.add(new Pair(0,0));
+23        ans[0]=0;
+24        while(!q.isEmpty()){
+25            Pair r=q.poll();
+26            int vtx=r.vtx;
+27            int cost=r.cost;
+28            if (cost > ans[vtx]) continue;
+29            if (cost >= disappear[vtx]) continue;
+30            for (Map.Entry<Integer, Integer> nbr : map.get(vtx).entrySet()) {
+31                int nbrVtx = nbr.getKey();
+32                int wt = nbr.getValue();
+33                int newTime = cost + wt;
+34                if (newTime < ans[nbrVtx] && newTime < disappear[nbrVtx]) {
+35                    ans[nbrVtx] = newTime;
+36                    q.add(new Pair(nbrVtx, newTime));
+37                }
+38            }
+39        }
+40        for (int i=0;i<n;i++) {
+41            if (ans[i]==Integer.MAX_VALUE) {
+42                ans[i]=-1;
+43            }
+44        }
+45        return ans;
+46    }
+47}
