3a00c3c94be58f19f481582d8dbc38f2156a7ed4
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-05 13:17:59 +0530

    File updated at 05/01/2026, 13:17:52

--- 64-MinimumPathSum/64-MinimumPathSum.java
+++ 64-MinimumPathSum/64-MinimumPathSum.java
@@ -1,18 +1,18 @@
-// Last updated: 10/09/2025, 11:57:47
-class Solution {
-    public int minPathSum(int[][] grid) {
-        int[][] dp=new int[grid.length][grid[0].length];
-        for(int[] arr:dp){
-            Arrays.fill(arr,-1);
-        }
-        return move(grid,0,0,dp);
-    }
-    public int move(int[][] grid,int r,int c,int[][] dp){
-        if(r==grid.length-1 && c==grid[0].length-1) return grid[r][c];
-        if(r==grid.length || c==grid[0].length) return Integer.MAX_VALUE;
-        if(dp[r][c]!=-1) return dp[r][c];
-        int mr=move(grid,r,c+1,dp);
-        int mb=move(grid,r+1,c,dp);
-        return dp[r][c]=Math.min(mr,mb)+grid[r][c];
-    }
-}
+// Last updated: 05/01/2026, 13:17:52
+1class Solution {
+2    public int minPathSum(int[][] grid) {
+3       int m=grid.length;
+4        int n=grid[0].length;
+5        int dp[][] =new int[m][n];
+6        for(int[] i:dp){
+7            Arrays.fill(i,Integer.MAX_VALUE);
+8        }
+9        return find(grid,m,n,0,0,dp);
+10    }
+11    public int find(int[][] grid,int m,int n,int i,int j,int[][] dp){
+12        if(i==m-1 && j==n-1) return grid[i][j];
+13        if(i>=m || j>=n) return Integer.MAX_VALUE;
+14        if(dp[i][j]!=Integer.MAX_VALUE) return dp[i][j];
+15        return dp[i][j]=grid[i][j]+Math.min(find(grid,m,n,i+1,j,dp),find(grid,m,n,i,j+1,dp));
+16    }
+17}
