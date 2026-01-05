06f826baa2e1c5f284abab756845e67dd15f03c5
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-05 11:49:02 +0530

    File updated at 05/01/2026, 11:48:55

--- 70-ClimbingStairs/70-ClimbingStairs.java
+++ 70-ClimbingStairs/70-ClimbingStairs.java
@@ -1,17 +1,14 @@
-// Last updated: 12/08/2025, 21:42:53
-class Solution {
-    private int cnt=0;
-    public int climbStairs(int n) {
-        int[] memo = new int[n + 1];
-        return climb(0,n,memo);
-    }
-    public int climb(int cl,int n,int[] memo){
-        if(cl==n) {
-            return 1;
-        }
-        if(cl>n) return 0;
-        if(memo[cl]!=0) return memo[cl];
-        memo[cl]=climb(cl+1,n,memo)+climb(cl+2,n,memo);
-        return memo[cl];
-    }
-}
+// Last updated: 05/01/2026, 11:48:55
+1class Solution {
+2    public int climbStairs(int n) {
+3        int[] dp=new int[n+1];
+4        Arrays.fill(dp,-1);
+5        return find(n,dp);
+6    }
+7    public int find(int n,int[] dp){
+8        if(n<0) return 0;
+9        if(n==0) return 1;
+10        if(dp[n]!=-1) return dp[n];
+11        return dp[n]=find(n-1,dp)+find(n-2,dp); 
+12    }
+13}
