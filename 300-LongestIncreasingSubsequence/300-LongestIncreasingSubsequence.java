5d3847d5bb1dab82f4f150baa0c602c16682b874
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-06 23:28:00 +0530

    File updated at 06/01/2026, 23:27:52

--- 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
+++ 300-LongestIncreasingSubsequence/300-LongestIncreasingSubsequence.java
@@ -1,16 +1,24 @@
-// Last updated: 12/10/2025, 00:55:19
-class Solution {
-    public int lengthOfLIS(int[] nums) {
-       int[] dp=new int[nums.length];
-       Arrays.fill(dp,1);
-       for(int i=1;i<dp.length;i++){
-        for(int j=i-1;j>=0;j--){
-            if(nums[i]>nums[j]){
-                int x=dp[j];
-                dp[i]=Math.max(dp[i],x+1);
-            }
-        }
-       }
-       return Arrays.stream(dp).max().getAsInt();
-    }
-}
+// Last updated: 06/01/2026, 23:27:52
+1class Solution {
+2    public int lengthOfLIS(int[] nums) {
+3        int n=nums.length;
+4        int dp[] =new int[n];
+5        Arrays.fill(dp,Integer.MIN_VALUE);
+6        int ans=Integer.MIN_VALUE;
+7        for(int i=0;i<nums.length;i++){
+8            ans=Math.max(ans,find(nums,i,dp));
+9        }
+10        return ans;
+11    }
+12    public int find(int[] nums,int idx,int[] dp){
+13        if(idx>=nums.length) return Integer.MIN_VALUE;
+14        if(dp[idx]!=Integer.MIN_VALUE) return dp[idx];
+15        int max=1;
+16        for(int i=0;i<idx;i++){
+17            if(nums[i]<nums[idx]){
+18                max=Math.max(max,1+find(nums,i,dp));
+19            }
+20        }
+21        return dp[idx]=max;
+22    }
+23}
