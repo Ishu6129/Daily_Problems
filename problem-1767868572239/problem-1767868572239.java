d53c431869f32d31ac08fea24d0eabf324e8a43e
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-08 16:06:21 +0530

    Create

---
+++ problem-1767868572239/problem-1767868572239.java
@@ -0,0 +1,20 @@
+// Last updated: 08/01/2026, 16:06:12
+1class Solution {
+2    public int maxDotProduct(int[] nums1, int[] nums2) {
+3        int[][] dp=new int[nums1.length][nums2.length];
+4        for(int[] i:dp){
+5            Arrays.fill(i,Integer.MIN_VALUE);
+6        }
+7        return find(nums1,nums2,0,0,dp);
+8    }
+9    public int find(int[] nums1,int[] nums2,int i,int j,int[][] dp){
+10        if(i>=nums1.length || j>=nums2.length){
+11            return Integer.MIN_VALUE;
+12        }
+13        if(dp[i][j]!=Integer.MIN_VALUE) return dp[i][j];
+14        int tk=nums1[i]*nums2[j]+Math.max(0,find(nums1,nums2,i+1,j+1,dp));
+15        int npk1=find(nums1,nums2,i+1,j,dp);
+16        int npk2=find(nums1,nums2,i,j+1,dp);
+17        return dp[i][j]=Math.max(tk,Math.max(npk1,npk2));
+18    }
+19}
