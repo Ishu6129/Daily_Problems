0e1af46241294512ab1befb6641e5b016eef055d
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-01-30 12:36:48 +0530

    File updated at 30/01/2026, 12:36:22

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
@@ -1,10 +1,20 @@
-// Last updated: 02/08/2025, 19:50:29
-class Solution {
-    public int[] sortedSquares(int[] nums) {
-        for(int i=0;i<nums.length;i++){
-            nums[i]=nums[i]*nums[i];
-        }
-        Arrays.sort(nums);
-        return nums;
-    }
-}
+// Last updated: 30/01/2026, 12:36:22
+1class Solution {
+2    public int[] sortedSquares(int[] nums) {
+3        int[] ans=new int[nums.length];
+4        int pointer=nums.length-1;
+5        int i=0;
+6        int j=nums.length-1;
+7        while(i<=j){
+8            if( (nums[j]*nums[j]) > (nums[i]*nums[i]) ){
+9                ans[pointer--]=(nums[j]*nums[j]);
+10                j--;
+11            }
+12            else{
+13               ans[pointer--]=(nums[i]*nums[i]);
+14                i++; 
+15            }
+16        }
+17        return ans;
+18    }
+19}
