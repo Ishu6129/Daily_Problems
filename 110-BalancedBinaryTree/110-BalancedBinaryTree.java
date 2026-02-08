2fa60d5fea7bfbbbfa06a75e63c98cb65deb37ed
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-08 20:55:15 +0530

    File updated at 08/02/2026, 20:54:42

--- 110-BalancedBinaryTree/110-BalancedBinaryTree.java
+++ 110-BalancedBinaryTree/110-BalancedBinaryTree.java
@@ -1,40 +1,34 @@
-// Last updated: 08/08/2025, 23:44:02
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isBalanced(TreeNode root) {
-        return isBalance(root).isBalanced;
-    }
-
-	private BalancedPair isBalance(TreeNode node) {
-        if(node==null){
-            BalancedPair sb=new BalancedPair();
-            sb.height=-1;
-            sb.isBalanced=true;
-            return sb;
-        }
-        BalancedPair lbp=isBalance(node.left);
-        BalancedPair rbp=isBalance(node.right);
-        BalancedPair sbp=new BalancedPair();
-        sbp.height=Math.max(lbp.height,rbp.height)+1;
-        sbp.isBalanced=Math.abs(lbp.height-rbp.height)<=1 && lbp.isBalanced && rbp.isBalanced;
-        return sbp;
-	}
-    private class BalancedPair {
-		int height;
-		boolean isBalanced;
-	}
-}
+// Last updated: 08/02/2026, 20:54:42
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    boolean flag=true;
+18    public boolean isBalanced(TreeNode root) {
+19        check(root);
+20        return flag;
+21    }
+22    public int check(TreeNode root){
+23        if(flag==false)return -1;
+24        if(root==null) return -1;
+25
+26        int lht=check(root.left);
+27        int rht=check(root.right);
+28        if(Math.abs(rht-lht)>1){
+29            flag=false;
+30        }
+31        return 1+Math.max(lht,rht);
+32    }
+33}
