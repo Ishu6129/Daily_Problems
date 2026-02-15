3661e7b7b8a8c6d21224b39b356ee1fdd95a4217
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-15 16:04:19 +0530

    File updated at 15/02/2026, 16:01:23

--- 67-AddBinary/67-AddBinary.java
+++ 67-AddBinary/67-AddBinary.java
@@ -1,41 +1,82 @@
-// Last updated: 31/07/2025, 19:40:51
-class Solution {
-    public String addBinary(String a, String b) {
-      String ans="";
-        int n=a.length()-1;
-        int m=b.length()-1;
-        if(m>n){
-            return rmz(add(b,a,ans));
-        }
-        return rmz(add(a,b,ans));
-    }
-    public String add(String s1,String s2,String ans){
-        int carry=0;
-        int sum=0;
-        int i=s1.length()-1;
-        int j=s2.length()-1;
-        while(j>=0){
-            int temp=carry+(s1.charAt(i)-'0')+(s2.charAt(j)-'0');
-            ans=((char)(temp%2+'0'))+ans;
-            carry=temp/2;
-            j--;i--;
-        }
-        while(i>=0){
-            int temp=carry+(s1.charAt(i)-'0');
-            ans=((char)(temp%2+'0'))+ans;
-            carry=temp/2;
-            i--;
-        }
-        if(carry==1){
-            return "1"+ans;
-        }
-        return ans;
-    }
-    public String rmz(String s){
-        int i=0;
-        while(i<s.length() && s.charAt(i)=='0'){
-            i++;
-        }
-        return i==s.length()?"0":s.substring(i);
-    }
-}
+// Last updated: 15/02/2026, 16:01:23
+1class Solution {
+2    public String addBinary(String a, String b) {
+3        StringBuilder sb = new StringBuilder();
+4        int a_len = a.length();
+5        int b_len = b.length();
+6        int l = Math.min(a_len, b_len);
+7        int c = 0;
+8
+9        // Process common length
+10        for (int i = 0; i < l; i++) {
+11            char a_ch = a.charAt(a_len - i - 1);
+12            char b_ch = b.charAt(b_len - i - 1);
+13
+14            if (a_ch == b_ch && a_ch == '0') {
+15                if (c == 0) {
+16                    sb.append('0');
+17                } else {
+18                    sb.append('1');
+19                    c = 0;
+20                }
+21            } 
+22            else if (a_ch == b_ch && a_ch == '1') {
+23                if (c == 0) {
+24                    sb.append('0');
+25                } else {
+26                    sb.append('1');
+27                }
+28                c = 1;
+29            } 
+30            else { // one is 0, one is 1
+31                if (c == 0) {
+32                    sb.append('1');
+33                } else {
+34                    sb.append('0');
+35                    c = 1;
+36                }
+37            }
+38        }
+39
+40        // Process remaining digits of longer string
+41        if (a_len > b_len) {
+42            for (int i = l; i < a_len; i++) {
+43                char ch = a.charAt(a_len - i - 1);
+44                if (c == 0) {
+45                    sb.append(ch);
+46                } else {
+47                    if (ch == '0') {
+48                        sb.append('1');
+49                        c = 0;
+50                    } else {
+51                        sb.append('0');
+52                        c = 1;
+53                    }
+54                }
+55            }
+56        } else {
+57            for (int i = l; i < b_len; i++) {
+58                char ch = b.charAt(b_len - i - 1);
+59                if (c == 0) {
+60                    sb.append(ch);
+61                } else {
+62                    if (ch == '0') {
+63                        sb.append('1');
+64                        c = 0;
+65                    } else {
+66                        sb.append('0');
+67                        c = 1;
+68                    }
+69                }
+70            }
+71        }
+72
+73        // Final carry
+74        if (c == 1) {
+75            sb.append('1');
+76        }
+77
+78        return sb.reverse().toString();
+79    }
+80}
+81
