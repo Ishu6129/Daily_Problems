fc15eb591fb7a350df54805b38b6cacaba4424a3
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-16 22:07:57 +0530

    File updated at 16/02/2026, 22:04:59

--- 190-ReverseBits/190-ReverseBits.java
+++ 190-ReverseBits/190-ReverseBits.java
@@ -1,10 +1,10 @@
-// Last updated: 31/07/2025, 19:34:24
-public class Solution {
-    public int reverseBits(int n) {
-        String str = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
-        StringBuilder sb = new StringBuilder(str);
-        sb.reverse();
-        long reversedLong = Long.parseLong(sb.toString(), 2);
-        return (int) reversedLong;
-    }
-}
+// Last updated: 16/02/2026, 22:04:59
+1public class Solution {
+2    public int reverseBits(int n) {
+3        String str = String.format("%32s", Integer.toBinaryString(n)).replace(' ', '0');
+4        StringBuilder sb = new StringBuilder(str);
+5        sb.reverse();
+6        long reversedLong = Long.parseLong(sb.toString(), 2);
+7        return (int) reversedLong;
+8    }
+9}
