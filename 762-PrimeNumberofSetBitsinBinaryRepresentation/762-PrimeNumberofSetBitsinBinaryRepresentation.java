97eb3b0293ba54a5b92f9ddcb6c7c52a728aa3e8
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-21 11:16:08 +0530

    Create 762-PrimeNumberofSetBitsinBinaryRepresentation

---
+++ 762-PrimeNumberofSetBitsinBinaryRepresentation/762-PrimeNumberofSetBitsinBinaryRepresentation.java
@@ -0,0 +1,19 @@
+// Last updated: 21/02/2026, 11:13:07
+1class Solution {
+2    static Set<Integer> set=Set.of(2,3,5,7,11,13,17,19);
+3    public int countPrimeSetBits(int left, int right) {
+4        int ans=0;
+5        for(int i=left;i<=right;i++){
+6            if(check(i)) ans++;
+7        }
+8        return ans;
+9    }
+10    public static boolean check(int n){
+11        int c=0;
+12        while(n>0){
+13            if((n&1)==1) c++;
+14            n=n>>1;
+15        }
+16        return set.contains(c);
+17    }
+18}
