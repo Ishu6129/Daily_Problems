c2974853defa0c80d65acd837f30c750ee6f4bad
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-22 15:56:54 +0530

    File updated at 22/02/2026, 15:53:52

--- 868-BinaryGap/868-BinaryGap.java
+++ 868-BinaryGap/868-BinaryGap.java
@@ -1,20 +1,21 @@
-// Last updated: 22/02/2026, 12:49:08
+// Last updated: 22/02/2026, 15:53:52
 1class Solution {
 2    public int binaryGap(int n) {
-3        String s=Integer.toBinaryString(n);
-4        int idx=0;
-5        while(idx<s.length() && s.charAt(idx)!='1'){
-6            idx++;
-7        }
-8        System.out.println(idx);
-9        if(idx==s.length()) return 0;
-10        int ans=0;
-11        for(int i=idx+1;i<s.length();i++){
-12            if(s.charAt(i)=='1'){
-13                ans=Math.max(i-idx,ans);
-14                idx=i;
-15            }
-16        }
-17        return ans;
-18    }
-19}
+3        while(n>0){
+4            if((n&1)==1) break;
+5            n=n>>1;
+6        }
+7        if(n<=0) return 0;
+8        int ans=0;
+9        int dis=0;
+10        while(n>0){
+11            if((n&1)==1){
+12                ans=Math.max(ans,dis);
+13                dis=0;
+14            }
+15            dis++;
+16            n=n>>1;
+17        } 
+18        return ans;
+19    }
+20}
