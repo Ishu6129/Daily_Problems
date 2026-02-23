2103beedad6c87d09bfe773bf640d571ac53db0e
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-23 15:37:56 +0530

    File updated at 23/02/2026, 15:34:53

--- 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.java
+++ 1461-CheckIfaStringContainsAllBinaryCodesofSizeK/1461-CheckIfaStringContainsAllBinaryCodesofSizeK.java
@@ -1,16 +1,17 @@
-// Last updated: 23/02/2026, 15:32:34
+// Last updated: 23/02/2026, 15:34:53
 1class Solution {
 2    public int[] minBitwiseArray(List<Integer> nums) {
 3        int[] ans=new int[nums.size()];
 4        for(int i=0;i<ans.length;i++){
-5            for(int j=1;j<=1000;j++){
-6                if( (j|(j+1))==nums.get(i) ){
-7                    ans[i]=j;
-8                    break;
-9                }
-10            }
-11            if(ans[i]==0) ans[i]=-1;
-12        }
-13        return ans;
-14    }
-15}
+5            int r=nums.get(i);
+6            for(int j=1;j<=r;j++){
+7                if( (j|(j+1))==nums.get(i) ){
+8                    ans[i]=j;
+9                    break;
+10                }
+11            }
+12            if(ans[i]==0) ans[i]=-1;
+13        }
+14        return ans;
+15    }
+16}
