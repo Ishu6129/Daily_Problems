73a4271de97a55604328072b8f68279ade7d761c
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-24 11:56:01 +0530

    File updated at 24/02/2026, 11:52:56

--- 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
+++ 144-BinaryTreePreorderTraversal/144-BinaryTreePreorderTraversal.java
@@ -1,34 +1,30 @@
-// Last updated: 06/08/2025, 01:42:19
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Integer> preorderTraversal(TreeNode root) {
-      List<Integer> ll=new ArrayList<>();
-        traverse(root,ll);
-        return ll;
-
-    }
-
-    public void traverse(TreeNode nn,List<Integer> ll){
-        
-        if(nn==null) return;
-
-        ll.add(nn.val);
-        traverse(nn.left,ll);
-        traverse(nn.right,ll);
-
-    }
-}
+// Last updated: 24/02/2026, 11:52:56
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public List<Integer> preorderTraversal(TreeNode root) {
+18        List<Integer> ans=new ArrayList<>();
+19        find(root,ans);
+20        return ans;
+21    }
+22    public void find(TreeNode root,List<Integer> ans){
+23        if(root==null) return;
+24        ans.add(root.val);
+25        find(root.left,ans);
+26        find(root.right,ans);
+27    }
+28
+29}
