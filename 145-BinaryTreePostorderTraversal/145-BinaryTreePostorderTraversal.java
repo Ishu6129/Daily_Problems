46c404c37ba9c47a4214b79e5cc7a7dce95a767e
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-24 12:01:31 +0530

    File updated at 24/02/2026, 11:58:26

--- 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.java
+++ 145-BinaryTreePostorderTraversal/145-BinaryTreePostorderTraversal.java
@@ -1,31 +1,26 @@
-// Last updated: 06/08/2025, 11:54:13
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<Integer> postorderTraversal(TreeNode root) {
-        List<Integer> ll=new  ArrayList<>();
-        traverse(root,ll);
-        return ll;
-    }
-
-    public void traverse(TreeNode nn,List<Integer> ll){
-        if(nn==null) return;
-
-        traverse(nn.left,ll);
-        traverse(nn.right,ll);
-        ll.add(nn.val);
-    }
-}
+// Last updated: 24/02/2026, 11:58:26
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    List<Integer> ans=new ArrayList<>();
+18    public List<Integer> postorderTraversal(TreeNode root) {
+19        if(root==null) return ans;
+20        postorderTraversal(root.left);
+21        postorderTraversal(root.right);
+22        ans.add(root.val);
+23        return ans;
+24    }
+25}
