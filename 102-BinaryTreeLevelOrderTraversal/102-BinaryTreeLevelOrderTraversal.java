b66ecb598256fa28e63504bc7a21cc4f025e44ca
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-24 12:12:03 +0530

    File updated at 24/02/2026, 12:08:59

--- 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
+++ 102-BinaryTreeLevelOrderTraversal/102-BinaryTreeLevelOrderTraversal.java
@@ -1,48 +1,41 @@
-// Last updated: 08/08/2025, 22:59:03
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public List<List<Integer>> levelOrder(TreeNode root) {
-        List<List<Integer>> ll = new ArrayList<>();
-        Queue<TreeNode> q = new LinkedList<>();
-        
-        if (root != null) {
-            q.add(root);
-        }
-        
-        while (!q.isEmpty()) {
-            int lsize = q.size();
-            List<Integer> arr = new ArrayList<>();
-            
-            int count = 0;
-            while (count < lsize) {
-                TreeNode n = q.poll();
-                arr.add(n.val);
-
-                if (n.left != null) {
-                    q.add(n.left);
-                }
-                if (n.right != null) {
-                    q.add(n.right);
-                }
-                count++;
-            }
-            
-            ll.add(arr);
-        }
-        return ll;
-    }
-}
+// Last updated: 24/02/2026, 12:08:59
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    List<List<Integer>> ans=new ArrayList<>();
+18    public List<List<Integer>> levelOrder(TreeNode root) {
+19        if(root==null) return ans;
+20        Queue<TreeNode> q=new LinkedList<>();
+21        q.add(root);
+22        while(!q.isEmpty()){
+23            int n=q.size();
+24            List<Integer> list=new ArrayList<>();
+25            for(int i=0;i<n;i++){
+26                TreeNode r=q.poll();
+27                list.add(r.val);
+28                if(r.left!=null){
+29                    q.add(r.left);
+30                }
+31                if(r.right!=null){
+32                    q.add(r.right);
+33                }
+34
+35            }
+36            ans.add(list);
+37        }
+38        return ans;
+39    }
+40}
