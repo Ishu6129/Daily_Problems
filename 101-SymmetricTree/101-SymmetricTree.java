474f6478a2cda42f5e058bbe36b03c9485654613
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-24 12:28:47 +0530

    File updated at 24/02/2026, 12:25:42

--- 101-SymmetricTree/101-SymmetricTree.java
+++ 101-SymmetricTree/101-SymmetricTree.java
@@ -1,30 +1,26 @@
-// Last updated: 13/08/2025, 00:19:20
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public boolean isSymmetric(TreeNode root) {
-        if(root==null) return true;
-        return mirrorIdentical(root.left, root.right);
-
-    }
-    private boolean mirrorIdentical(TreeNode tnode, TreeNode onode) {
-		if(tnode==null && onode==null) return true;
-        if(tnode==null || onode==null) return false;
-        if(tnode.val!=onode.val) {return false;}
-        return mirrorIdentical(tnode.left, onode.right) && 
-		mirrorIdentical(tnode.right, onode.left);
-	}
-}
+// Last updated: 24/02/2026, 12:25:42
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public boolean isSymmetric(TreeNode root) {
+18        return check(root.left,root.right);
+19    }
+20    public boolean check(TreeNode p,TreeNode q){
+21        if(q==null && p==null) return true;
+22        if(q==null||p==null||q.val!=p.val) return false;
+23        return check(p.left,q.right) && check(p.right,q.left);
+24    }
+25}
