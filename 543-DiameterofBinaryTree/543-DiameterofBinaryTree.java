f879e5db20a053a95049335e3717d143c7166c32
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-02-24 15:21:49 +0530

    File updated at 24/02/2026, 15:18:45

--- 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
+++ 543-DiameterofBinaryTree/543-DiameterofBinaryTree.java
@@ -1,36 +1,30 @@
-// Last updated: 31/07/2025, 23:00:29
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    private int maxDiameter = 0;
-    
-    public int diameterOfBinaryTree(TreeNode root) {
-        calculateHeight(root);
-        return maxDiameter;
-    }
-    
-    private int calculateHeight(TreeNode root) {
-        if (root == null) return -1;
-        
-        int leftHeight = calculateHeight(root.left);
-        int rightHeight = calculateHeight(root.right);
-        
-        int currentDiameter = leftHeight + rightHeight+2;
-        maxDiameter = Math.max(maxDiameter, currentDiameter);
-        
-        return Math.max(leftHeight, rightHeight) + 1;
-    }
-}
+// Last updated: 24/02/2026, 15:18:45
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }  
+14 * }
+15 */
+16class Solution {
+17    int d=0;
+18    public int diameterOfBinaryTree(TreeNode root) {
+19        find(root);
+20        return d;
+21    }
+22    public int find(TreeNode root){
+23        if(root==null) return 0;
+24        int l=find(root.left);
+25        int r=find(root.right);
+26        d=Math.max(d,l+r);
+27        return 1+Math.max(l,r);
+28    }
+29}
