15ca88589b2e4216b3cba663c2b17492de0024a8
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-03-04 22:48:50 +0530

    File updated at 04/03/2026, 22:45:38

--- 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
+++ 1004-MaxConsecutiveOnesIII/1004-MaxConsecutiveOnesIII.java
@@ -1,23 +1,17 @@
-// Last updated: 01/10/2025, 00:43:24
-class Solution {
-    public int longestOnes(int[] nums, int k) {
-        int[] freq=new int[2];
-        int i=0;
-        int j=0;
-        int max=0;
-        int ans=0;
-        while(j<nums.length){
-            freq[nums[j]]++;
-            max=Math.max(max,freq[1]);
-            int size=(j-i+1);
-            if(freq[0]>k){
-                freq[nums[i]]--;
-                i++;
-            }
-            size=j-i+1;
-            ans=Math.max(ans,size);
-            j++;
-        }
-        return ans;
-    }
-}
+// Last updated: 04/03/2026, 22:45:38
+1class Solution {
+2    public int longestOnes(int[] nums, int k) {
+3        int ans=0;
+4        int i=0;
+5        int c=0;
+6        for(int j=0;j<nums.length;j++){
+7            if(nums[j]==0) c++;
+8            while(c>k){
+9                if(nums[i]==0) c--;
+10                i++;
+11            }
+12            ans=Math.max(ans,j-i+1);
+13        }
+14        return ans;
+15    }
+16}
