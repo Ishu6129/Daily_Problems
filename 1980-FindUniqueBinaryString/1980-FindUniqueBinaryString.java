1b775fb27bafa40efb5b26f1ebdcd40c1a44edfc
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-03-08 15:37:27 +0530

    File updated at 08/03/2026, 15:34:11

--- 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.java
+++ 1980-FindUniqueBinaryString/1980-FindUniqueBinaryString.java
@@ -1,15 +1,17 @@
-// Last updated: 08/03/2026, 15:21:00
+// Last updated: 08/03/2026, 15:34:11
 1class Solution {
 2    public String findDifferentBinaryString(String[] nums) {
 3        Set<Integer> set=new HashSet<>();
 4        for(String s:nums){
 5            set.add(Integer.parseInt(s,2));
 6        }
-7        for(int i=0;i<nums.length;i++){
+7        for(int i=0;i<=nums.length;i++){
 8            if(!set.contains(i)){
-9                return String.format("%"+nums[0].length()+"s",Integer.toBinaryString(i)).replace(' ', '0');
-10            }
-11        }
-12        return String.format("%"+nums[0].length()+"s",Integer.toBinaryString(nums.length)).replace(' ', '0');
-13    }
-14}
+9                return String.format("%"+nums[0].length()+"s",
+10                                    Integer.toBinaryString(i))
+11                                    .replace(' ', '0');
+12            }
+13        }
+14        return "";
+15    }
+16}
