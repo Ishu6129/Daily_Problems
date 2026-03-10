270a0d5fa2fcf6b28f8518264d7319e9e912161e
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-03-10 11:07:16 +0530

    File updated at 10/03/2026, 11:03:57

--- 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
+++ 103-BinaryTreeZigzagLevelOrderTraversal/103-BinaryTreeZigzagLevelOrderTraversal.java
@@ -1,48 +1,45 @@
-// Last updated: 10/03/2026, 10:58:37
-1/**
-2 * Definition for a binary tree node.
-3 * public class TreeNode {
-4 *     int val;
-5 *     TreeNode left;
-6 *     TreeNode right;
-7 *     TreeNode() {}
-8 *     TreeNode(int val) { this.val = val; }
-9 *     TreeNode(int val, TreeNode left, TreeNode right) {
-10 *         this.val = val;
-11 *         this.left = left;
-12 *         this.right = right;
-13 *     }
-14 * }
-15 */
-16class Solution {
-17    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
-18        List<List<Integer>> ans=new ArrayList<>();
-19        if(root==null) return ans;
-20        Queue<TreeNode> q=new LinkedList<>();
-21        q.add(root);
-22        int level=-1;
-23        while(!q.isEmpty()){
-24            int s=q.size();
-25            level++;
-26            List<Integer> ls=new ArrayList<>();
-27            for(int i=0;i<s;i++){
-28                TreeNode r=q.poll();
-29                ls.add(r.val);
-30                if(r.left!=null){
-31                    q.add(r.left);
-32                }
-33                if(r.right!=null){
-34                    q.add(r.right);
-35                }
-36            }
-37            if(level%2==0){
-38                ans.add(ls);
-39            }
-40            else{
-41                Collections.reverse(ls);
-42                ans.add(ls);
-43            }
-44        }
-45        return ans;
-46    }
-47}
+// Last updated: 10/03/2026, 11:03:57
+import java.util.*;
+
+class Solution {
+
+    public List<List<Integer>> zigzagLevelOrder(TreeNode root) {
+
+        List<List<Integer>> result = new ArrayList<>();
+
+        if(root == null)
+            return result;
+
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(root);
+
+        boolean leftToRight = true;
+
+        while(!queue.isEmpty()){
+
+            int size = queue.size();
+            LinkedList<Integer> level = new LinkedList<>();
+
+            for(int i = 0; i < size; i++){
+
+                TreeNode node = queue.poll();
+
+                if(leftToRight)
+                    level.addLast(node.val);
+                else
+                    level.addFirst(node.val);
+
+                if(node.left != null)
+                    queue.add(node.left);
+
+                if(node.right != null)
+                    queue.add(node.right);
+            }
+
+            result.add(level);
+            leftToRight = !leftToRight;
+        }
+
+        return result;
+    }
+}
