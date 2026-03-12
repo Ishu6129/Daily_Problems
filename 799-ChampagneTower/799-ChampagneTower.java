55c84fe8ba479796d8a93ebcc56f781a41047be0
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-03-12 23:41:54 +0530

    File updated at 12/03/2026, 23:23:49

--- 799-ChampagneTower/799-ChampagneTower.java
+++ 799-ChampagneTower/799-ChampagneTower.java
@@ -1,33 +1,19 @@
-// Last updated: 09/03/2026, 11:48:08
+// Last updated: 12/03/2026, 23:23:49
 1class Solution {
-2    public int minimumPairRemoval(int[] nums) {
-3        ArrayList<Integer> arr = new ArrayList<>();
-4        for (int num : nums) {
-5            arr.add(num);
-6        }
-7        if(check(arr)) return 0;
-8        int ans=0;
-9        while(arr.size()>0){
-10            int[] fnd={-1,Integer.MAX_VALUE};
-11            for(int i=0;i<arr.size()-1;i++){
-12                int sum=arr.get(i)+arr.get(i+1);
-13                if(sum<fnd[1]){
-14                    fnd[0]=i;
-15                    fnd[1]=sum;
-16                }
-17            }
-18            arr.remove(fnd[0]);
-19            arr.remove(fnd[0]);
-20            arr.add(fnd[0],fnd[1]);
-21            ans++;
-22            if(check(arr)) return ans;
-23        }
-24        return 0;
-25    }
-26    public boolean check(ArrayList<Integer> arr){
-27        for(int i=0;i<arr.size()-1;i++){
-28            if(arr.get(i)>arr.get(i+1)) return false;
-29        }
-30        return true;
-31    }
-32}
+2    public double champagneTower(int poured, int query_row, int query_glass) {
+3        double[][] dp = new double[101][101];
+4        dp[0][0] = poured;
+5        for(int r=0;r<=query_row;r++){
+6            for(int c=0;c<=r;c++){
+7                if(dp[r][c]>1){
+8                    double of=(dp[r][c]-1)/2.0;
+9                    dp[r][c]=1;
+10                    dp[r+1][c]+=of;
+11                    dp[r+1][c+1]+=of;
+12                }
+13            }
+14        }
+15        return Math.min(1,dp[query_row][query_glass]);
+16
+17    }
+18}
