8fba6c1be2bfb3589ab2a8c2cf1ea9389588f76d
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-03-17 12:25:39 +0530

    File updated at 17/03/2026, 12:25:33

--- 1161-MaximumLevelSumofaBinaryTree/1161-MaximumLevelSumofaBinaryTree.java
+++ 1161-MaximumLevelSumofaBinaryTree/1161-MaximumLevelSumofaBinaryTree.java
@@ -1,50 +1,46 @@
-// Last updated: 09/08/2025, 23:50:02
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
-class Solution {
-    public int maxLevelSum(TreeNode root) {
-        int maxLevel=0;
-        int maxSum=Integer.MIN_VALUE;
-        Queue<TreeNode> q = new LinkedList<>();
-        if (root != null) {
-            q.add(root);
-        }
-        int currentLevel=1;
-        while (!q.isEmpty()) {
-            int lsize = q.size();
-            int sum=0;
-            int count = 0;
-            while (count < lsize) {
-                TreeNode n = q.poll();
-                sum+=n.val;
-
-                if (n.left != null) {
-                    q.add(n.left);
-                }
-                if (n.right != null) {
-                    q.add(n.right);
-                }
-                count++;
-            }
-            if(maxSum<sum){
-                maxSum=sum;
-                maxLevel=currentLevel;
-            }
-            currentLevel++;
-        }
-        return maxLevel;
-    }
-}
+// Last updated: 17/03/2026, 12:25:33
+1/**
+2 * Definition for a binary tree node.
+3 * public class TreeNode {
+4 *     int val;
+5 *     TreeNode left;
+6 *     TreeNode right;
+7 *     TreeNode() {}
+8 *     TreeNode(int val) { this.val = val; }
+9 *     TreeNode(int val, TreeNode left, TreeNode right) {
+10 *         this.val = val;
+11 *         this.left = left;
+12 *         this.right = right;
+13 *     }
+14 * }
+15 */
+16class Solution {
+17    public int maxLevelSum(TreeNode root) {
+18        Queue<TreeNode> q=new LinkedList<>();
+19        int level=1;
+20        int ans =1;
+21        int maxSum=Integer.MIN_VALUE;
+22        q.add(root);
+23        while(!q.isEmpty()){
+24            int qSize=q.size();
+25            int sum=0;
+26            for(int i=0;i<qSize;i++){
+27                TreeNode r=q.poll();
+28                sum+=r.val;
+29                if(r.left!=null){
+30                    q.add(r.left);
+31                }
+32                if(r.right!=null){
+33                    q.add(r.right);
+34                }
+35            }
+36            if(maxSum<sum){
+37                ans=level;
+38                maxSum=sum;   
+39            }
+40            level++;
+41        }
+42        return ans;
+43
+44    }
+45}
