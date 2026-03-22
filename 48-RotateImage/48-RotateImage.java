c625cf9aeaed0e64e7fd305d23b4c9c41208df4b
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-03-22 18:14:01 +0530

    File updated at 22/03/2026, 18:13:50

--- 48-RotateImage/48-RotateImage.java
+++ 48-RotateImage/48-RotateImage.java
@@ -1,20 +1,15 @@
-// Last updated: 06/08/2025, 00:41:05
-class Solution {
-    public void rotate(int[][] matrix) {
-        int n=matrix.length;
-        for(int row=0;row<n;row++){
-            for(int col=row+1;col<n;col++){
-                int temp = matrix[row][col];
-                matrix[row][col] = matrix[col][row];
-                matrix[col][row] = temp;
-            }
-        }
-        for (int row = 0; row < n; row++) {
-            for (int col = 0; col < n / 2; col++) {
-                int temp = matrix[row][col];
-                matrix[row][col] = matrix[row][n - 1 - col];
-                matrix[row][n - 1 - col] = temp;
-            }
-        }
-    }
-}
+// Last updated: 22/03/2026, 18:13:50
+1class Solution {
+2    public void rotate(int[][] mat) {
+3        int n=mat.length;
+4        for(int i=0;i<n/2;i++){
+5            for(int j=i;j<n-i-1;j++){
+6              int temp=mat[i][j];
+7              mat[i][j]=mat[n-j-1][i];
+8              mat[n-j-1][i]=mat[n-i-1][n-j-1];
+9              mat[n-i-1][n-j-1]=mat[j][n-i-1];
+10              mat[j][n-i-1]=temp;
+11            }
+12        }
+13    }
+14}
