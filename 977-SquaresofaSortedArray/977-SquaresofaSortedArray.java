da18aa83cbf0c45ae629fc91345c69930f7ca0e0
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-03-26 16:45:29 +0530

    File updated at 26/03/2026, 16:45:13

--- 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
+++ 977-SquaresofaSortedArray/977-SquaresofaSortedArray.java
@@ -1,20 +1,6 @@
-// Last updated: 30/01/2026, 12:36:22
+// Last updated: 26/03/2026, 16:45:13
 1class Solution {
 2    public int[] sortedSquares(int[] nums) {
-3        int[] ans=new int[nums.length];
-4        int pointer=nums.length-1;
-5        int i=0;
-6        int j=nums.length-1;
-7        while(i<=j){
-8            if( (nums[j]*nums[j]) > (nums[i]*nums[i]) ){
-9                ans[pointer--]=(nums[j]*nums[j]);
-10                j--;
-11            }
-12            else{
-13               ans[pointer--]=(nums[i]*nums[i]);
-14                i++; 
-15            }
-16        }
-17        return ans;
-18    }
-19}
+3        return Arrays.stream(nums).map(x->x*x).sorted().toArray();
+4    }
+5}
