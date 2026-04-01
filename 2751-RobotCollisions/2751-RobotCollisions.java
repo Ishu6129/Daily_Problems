e3a377d51e77a590c22fcedcdd50b680c05ebd11
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-04-01 21:40:08 +0530

    File updated at 01/04/2026, 21:39:45

--- 2751-RobotCollisions/2751-RobotCollisions.java
+++ 2751-RobotCollisions/2751-RobotCollisions.java
@@ -1,4 +1,4 @@
-// Last updated: 01/04/2026, 21:24:12
+// Last updated: 01/04/2026, 21:39:45
 1class Solution {
 2    class Pair{
 3        int pos;
@@ -11,48 +11,44 @@
 10        }
 11    }
 12    public List<Integer> survivedRobotsHealths(int[] positions, int[] healths, String directions) {
-13        PriorityQueue<Pair> pq=new PriorityQueue<>((a,b)->a.pos-b.pos);
-14        int n=positions.length;
+13        int n=positions.length;
+14        Pair[] arr=new Pair[n];
 15        for(int i=0;i<n;i++){
-16            pq.add(new Pair(positions[i],healths[i],directions.charAt(i)));
+16            arr[i]=new Pair(positions[i],healths[i],directions.charAt(i));
 17        }
-18        Pair[] arr=new Pair[n];
-19        int idx=0;
-20        for(int i=0;i<n;i++){
-21            arr[idx++]=pq.poll();
-22        }
-23        HashMap<Integer,Integer> map=new HashMap<>();
-24        Stack<Pair> mid = new Stack<>();
-25        for(int i=0;i<n;i++){
-26            Pair curr=arr[i];
-27            boolean collide=false;
-28            while (!mid.isEmpty() && curr.dir=='L' && mid.peek().dir=='R') {
-29                if (curr.hlth>mid.peek().hlth){
-30                    curr.hlth-=1;
-31                    mid.pop();
-32                } 
-33                else if (curr.hlth==mid.peek().hlth) {
-34                    mid.pop();
-35                    collide = true;
-36                    break;
-37                }
-38                else {
-39                    mid.peek().hlth-=1;
-40                    collide = true;
-41                    break;
-42                }
-43            }
-44            if (!collide) {
-45                mid.push(curr);
-46            }
-47        }
-48        for (Pair ele:mid) map.put(ele.pos,ele.hlth);
-49        List<Integer> ans=new ArrayList<>();
-50        for(int i:positions){
-51            if(map.containsKey(i)){
-52                ans.add(map.get(i));
-53            }
-54        }
-55        return ans;
-56    }
-57}
+18        Arrays.sort(arr,(a,b)->a.pos-b.pos);
+19        HashMap<Integer,Integer> map=new HashMap<>();
+20        Stack<Pair> st = new Stack<>();
+21        for(int i=0;i<n;i++){
+22            Pair curr=arr[i];
+23            boolean collide=false;
+24            while (!st.isEmpty() && curr.dir=='L' && st.peek().dir=='R') {
+25                if (curr.hlth>st.peek().hlth){
+26                    curr.hlth-=1;
+27                    st.pop();
+28                } 
+29                else if (curr.hlth==st.peek().hlth) {
+30                    st.pop();
+31                    collide = true;
+32                    break;
+33                }
+34                else {
+35                    st.peek().hlth-=1;
+36                    collide = true;
+37                    break;
+38                }
+39            }
+40            if (!collide) {
+41                st.push(curr);
+42            }
+43        }
+44        for (Pair ele:st) map.put(ele.pos,ele.hlth);
+45        List<Integer> ans=new ArrayList<>();
+46        for(int i:positions){
+47            if(map.containsKey(i)){
+48                ans.add(map.get(i));
+49            }
+50        }
+51        return ans;
+52    }
+53}
