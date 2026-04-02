35d836adc78c8b68b05ba650fd2dcd6a5453eb85
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-04-02 19:23:53 +0530

    File updated at 02/04/2026, 19:23:30

--- 3418-MaximumAmountofMoneyRobotCanEarn/3418-MaximumAmountofMoneyRobotCanEarn.java
+++ 3418-MaximumAmountofMoneyRobotCanEarn/3418-MaximumAmountofMoneyRobotCanEarn.java
@@ -1,41 +1,37 @@
-// Last updated: 02/04/2026, 11:55:32
+// Last updated: 02/04/2026, 19:23:30
 1class Solution {
-2    int m,n;
-3    int[][][] dp;
-4    public int maximumAmount(int[][] coins) {
-5        m=coins.length;
-6        n=coins[0].length;
-7        dp=new int[m][n][3];
-8        for(int[][] i:dp){
-9            for(int[] j:i){
-10                Arrays.fill(j,Integer.MIN_VALUE/2);
-11            }
-12        }
-13        return find(0,0,2,coins);
-14    }
-15    int[] row={0,1};
-16    int[] col={1,0};
-17    public int find(int i,int j,int shield,int[][] arr){
-18        if(i>=m || j>=n){
-19            return Integer.MIN_VALUE/2;
+2    public int maximumAmount(int[][] coins) {
+3        int m=coins.length;
+4        int n=coins[0].length;
+5        int[][][] dp=new int[m+1][n+1][3];
+6        for(int[][] i:dp){
+7            for(int[] j:i){
+8                Arrays.fill(j,Integer.MIN_VALUE/2);
+9            }
+10        }
+11        if(coins[m-1][n-1]<0){
+12            dp[m-1][n-1][2]=0;
+13            dp[m-1][n-1][1]=0;
+14            dp[m-1][n-1][0]=coins[m-1][n-1];
+15        }
+16        else{
+17            dp[m-1][n-1][2]=coins[m-1][n-1];
+18            dp[m-1][n-1][1]=coins[m-1][n-1];
+19            dp[m-1][n-1][0]=coins[m-1][n-1];
 20        }
-21        if(i==m-1 && j==n-1){
-22            if(shield>0 && arr[i][j]<0){
-23                return 0;
-24            }
-25            return arr[i][j];
-26        }
-27        if(dp[i][j][shield]!=Integer.MIN_VALUE/2) return dp[i][j][shield];
-28        int ans=Integer.MIN_VALUE/2;
-29        for(int d=0;d<2;d++){
-30            int skp=Integer.MIN_VALUE/2;
-31            if(shield>0 && arr[i][j]<0){
-32                skp=find(i+row[d],j+col[d],shield-1,arr);
-33            }
-34            
-35            int nskp=arr[i][j]+find(i+row[d],j+col[d],shield,arr);
-36            ans = Math.max(ans, Math.max(skp, nskp));
-37        }
-38        return dp[i][j][shield]=ans;
-39    }
-40}
+21        for(int i=m-1;i>=0;i--){
+22            for(int j=n-1;j>=0;j--){
+23                if(i==m-1 && j==n-1) continue;
+24                for(int shield=0;shield<=2;shield++){
+25                    int skp=Integer.MIN_VALUE/2;
+26                    if(shield>0 && coins[i][j]<0){
+27                        skp=Math.max(dp[i][j+1][shield-1],dp[i+1][j][shield-1]);
+28                    }
+29                    int nskp=coins[i][j]+Math.max(dp[i][j+1][shield],dp[i+1][j][shield]);
+30                    dp[i][j][shield]=Math.max(dp[i][j][shield],Math.max(skp,nskp));
+31                }
+32            }
+33        }
+34        return dp[0][0][2];
+35    }
+36}
