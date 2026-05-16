c745a8b10150a463c8f0c73259e2458c084c8131
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-05-16 13:04:37 +0530

    File updated at 5/16/2026, 1:04:32 PM

--- 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
+++ 154-FindMinimuminRotatedSortedArrayII/154-FindMinimuminRotatedSortedArrayII.java
@@ -1,7 +1,24 @@
-// Last updated: 05/12/2025, 12:58:00
+// Last updated: 5/16/2026, 1:04:32 PM
 1class Solution {
 2    public int findMin(int[] nums) {
-3        Arrays.sort(nums);
-4        return nums[0];
-5    }
-6}
+3        int i=0;
+4        int j=nums.length-1;
+5        int min=Integer.MAX_VALUE;
+6        while(i<=j){
+7            int mid=i+(j-i)/2;
+8            if(nums[i]<nums[mid]){
+9                min=Math.min(min,nums[i]);
+10                i=mid+1;
+11            }
+12            else if(nums[i]>nums[mid]){
+13                min=Math.min(min,nums[mid]);
+14                j=mid-1;
+15            }
+16            else{
+17                min = Math.min(min, nums[i]);
+18                i++;
+19            }
+20        }
+21        return min;
+22    }
+23}
