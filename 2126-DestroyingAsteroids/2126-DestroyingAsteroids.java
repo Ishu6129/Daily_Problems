1fca4ca1e8a83b772cbc713c6b2d202d807ac91b
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-05-31 19:44:26 +0530

    File updated at 5/31/2026, 7:44:26 PM

--- 2126-DestroyingAsteroids/2126-DestroyingAsteroids.java
+++ 2126-DestroyingAsteroids/2126-DestroyingAsteroids.java
@@ -1,13 +1,13 @@
-// Last updated: 5/31/2026, 7:42:59 PM
+// Last updated: 5/31/2026, 7:44:26 PM
 1class Solution {
 2    public boolean asteroidsDestroyed(int mass, int[] asteroids) {
 3        PriorityQueue<Integer> q=new PriorityQueue<>();
 4        for(int i:asteroids) q.add(i);
-5        long ms=(long)mass;
+5        long ms=mass;
 6        while(!q.isEmpty()){
 7            int ast=q.poll();
-8            if(((long)ast)>ms) return false;
-9            ms+=(long)ast;
+8            if(ast>ms) return false;
+9            ms+=ast;
 10        }
 11        return true;
 12    }
