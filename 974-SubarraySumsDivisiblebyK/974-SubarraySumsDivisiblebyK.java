af1a537e4c7ca62e262421e9d739483381ddaab6
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-06-02 00:41:43 +0530

    File updated at 6/2/2026, 12:41:42 AM

--- 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.java
+++ 974-SubarraySumsDivisiblebyK/974-SubarraySumsDivisiblebyK.java
@@ -1,15 +1,15 @@
-// Last updated: 6/2/2026, 12:39:42 AM
+// Last updated: 6/2/2026, 12:41:42 AM
 1class Solution {
 2    public int subarraysDivByK(int[] nums, int k) {
-3        Map<Integer,Long> freq=new HashMap<>();
-4        freq.put(0,1L);
+3        int[] freq=new int[10001];
+4        freq[0]=1;
 5        int ans=0;
 6        long pre=0L;
 7        for(int num:nums){
 8            pre+=num;
 9            int rem=(int)(((pre%k)+k)%k);
-10            ans+=freq.getOrDefault(rem,0L);
-11            freq.put(rem,freq.getOrDefault(rem,0L)+1);
+10            ans+=freq[rem];
+11            freq[rem]++;
 12        }
 13        return (int) ans;
 14    }
