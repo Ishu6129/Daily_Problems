4871766417f025791fc0a63bcd349c7c62b1c517
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-06-07 20:10:35 +0530

    File updated at 6/7/2026, 8:10:35 PM

--- 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
+++ 560-SubarraySumEqualsK/560-SubarraySumEqualsK.java
@@ -1,18 +1,18 @@
-// Last updated: 12/10/2025, 00:54:59
-class Solution {
-    public int subarraySum(int[] nums, int k) {
-        Map<Integer,Integer> map=new HashMap<>();
-        int pre=0;
-        int cnt=0;
-        map.put(0,1);
-        for(int i=0;i<nums.length;i++){
-            pre+=nums[i];
-            int rm=pre-k;
-            if(map.containsKey(rm)){
-                cnt+= map.get(rm);
-            }
-            map.put(pre, map.getOrDefault(pre, 0) + 1);
-        }
-        return cnt;
-    }
-}
+// Last updated: 6/7/2026, 8:10:35 PM
+1class Solution {
+2    public int subarraySum(int[] nums, int k) {
+3        Map<Integer,Integer> map=new HashMap<>();
+4        int pre=0;
+5        int cnt=0;
+6        map.put(0,1);
+7        for(int i=0;i<nums.length;i++){
+8            pre+=nums[i];
+9            int rm=pre-k;
+10            if(map.containsKey(rm)){
+11                cnt+= map.get(rm);
+12            }
+13            map.put(pre, map.getOrDefault(pre, 0) + 1);
+14        }
+15        return cnt;
+16    }
+17}
