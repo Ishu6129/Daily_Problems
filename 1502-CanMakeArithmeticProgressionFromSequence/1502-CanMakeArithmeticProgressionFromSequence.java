1e64583a704b51434de56254befa3ff78f7e598b
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-07-10 19:09:36 +0530

    File updated at 7/10/2026, 7:09:34 PM

--- 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.java
+++ 1502-CanMakeArithmeticProgressionFromSequence/1502-CanMakeArithmeticProgressionFromSequence.java
@@ -1,13 +1,12 @@
-// Last updated: 7/10/2026, 7:09:20 PM
+// Last updated: 7/10/2026, 7:09:34 PM
 1class Solution {
 2    public boolean canMakeArithmeticProgression(int[] arr) {
 3        Arrays.sort(arr);
 4        int n=arr.length;
-5        if(n<=2) return true;
-6        int diff=arr[1]-arr[0];
-7        for(int i=1;i<n-1;i++){
-8            if((arr[i+1]-arr[i])!=diff) return false;
-9        }
-10        return true;
-11    }
-12}
+5        int diff=arr[1]-arr[0];
+6        for(int i=1;i<n-1;i++){
+7            if((arr[i+1]-arr[i])!=diff) return false;
+8        }
+9        return true;
+10    }
+11}
