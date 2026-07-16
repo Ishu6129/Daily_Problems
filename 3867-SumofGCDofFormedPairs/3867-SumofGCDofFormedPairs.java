28d80cd545c8bf18340633664883956827a5b3ac
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-07-16 09:15:41 +0530

    File updated at 7/16/2026, 9:15:40 AM

--- 3867-SumofGCDofFormedPairs/3867-SumofGCDofFormedPairs.java
+++ 3867-SumofGCDofFormedPairs/3867-SumofGCDofFormedPairs.java
@@ -1,4 +1,4 @@
-// Last updated: 7/16/2026, 9:15:32 AM
+// Last updated: 7/16/2026, 9:15:40 AM
 1class Solution {
 2    public int gcd(int a,int b){
 3        if(b==0) return a;
@@ -11,14 +11,13 @@
 10            if(mx<nums[i]) mx=nums[i];
 11            nums[i]=gcd(mx,nums[i]);
 12        }
-13        System.out.println(Arrays.toString(nums));
-14        Arrays.sort(nums);
-15        long ans=0;
-16        int end=n/2;
-17        for(int i=0;i<end;i++){
-18            int gc=gcd(nums[i],nums[n-i-1]);
-19            ans+=gc;
-20        }
-21        return ans;
-22    }
-23}
+13        Arrays.sort(nums);
+14        long ans=0;
+15        int end=n/2;
+16        for(int i=0;i<end;i++){
+17            int gc=gcd(nums[i],nums[n-i-1]);
+18            ans+=gc;
+19        }
+20        return ans;
+21    }
+22}
