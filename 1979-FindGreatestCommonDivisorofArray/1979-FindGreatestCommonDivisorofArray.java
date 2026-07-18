7eec0a3b6c3fe8e8259f682e89ab0f4e8eb66415
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-07-18 12:21:15 +0530

    File updated at 7/18/2026, 12:21:11 PM

--- 1979-FindGreatestCommonDivisorofArray/1979-FindGreatestCommonDivisorofArray.java
+++ 1979-FindGreatestCommonDivisorofArray/1979-FindGreatestCommonDivisorofArray.java
@@ -1,17 +1,11 @@
-// Last updated: 20/03/2026, 01:22:12
-class Solution {
-    public int findGCD(int[] nums) {
-        int min=10001,max=0;
-        int ans=1;
-        for(int i:nums){
-            if(min>i) min=i;
-            if(max<i) max=i;
-        }
-        for(int i=2;i<=max;i++){
-            if(max%i==0 && min%i==0){
-                ans=i;
-            }
-        }
-        return ans;
-    }
-}
+// Last updated: 7/18/2026, 12:21:11 PM
+1class Solution {
+2    public int gcd(int a ,int b){
+3        if(b==0) return a;
+4        return gcd(b,a%b);
+5    }
+6    public int findGCD(int[] nums) {
+7        Arrays.sort(nums);
+8        return gcd(nums[0],nums[nums.length-1]);
+9    }
+10}
