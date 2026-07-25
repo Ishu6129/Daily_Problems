2d9e7789f5f52dd7c7ee600864201a9cc6c14d7e
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-07-25 12:58:58 +0530

    File updated at 7/25/2026, 12:58:56 PM

--- 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.java
+++ 3536-MaximumProductofTwoDigits/3536-MaximumProductofTwoDigits.java
@@ -1,12 +1,21 @@
-// Last updated: 7/25/2026, 12:56:39 PM
-1class Solution {
-2    public int maxProduct(int n) {
-3        PriorityQueue<Integer> pq=new PriorityQueue<>(Collections.reverseOrder());
-4        int t=n;
-5        while(t>0){
-6            pq.add(t%10);
-7            t/=10;
-8        }
-9        return pq.poll()*pq.poll();
-10    }
-11}
+// Last updated: 7/25/2026, 12:58:56 PM
+class Solution {
+    public int maxProduct(int n) {
+        int max1 = 0, max2 = 0;
+
+        while (n > 0) {
+            int digit = n % 10;
+
+            if (digit > max1) {
+                max2 = max1;
+                max1 = digit;
+            } else if (digit > max2) {
+                max2 = digit;
+            }
+
+            n /= 10;
+        }
+
+        return max1 * max2;
+    }
+}
