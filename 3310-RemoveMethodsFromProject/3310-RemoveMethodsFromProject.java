7accd17556671818a21c8de04c7e212d5bc28eb3
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-08-05 17:38:58 +0530

    File updated at 8/5/2026, 5:38:58 PM

--- 3310-RemoveMethodsFromProject/3310-RemoveMethodsFromProject.java
+++ 3310-RemoveMethodsFromProject/3310-RemoveMethodsFromProject.java
@@ -1,4 +1,4 @@
-// Last updated: 8/5/2026, 5:32:43 PM
+// Last updated: 8/5/2026, 5:38:58 PM
 1class Solution {
 2    public List<Integer> remainingMethods(int n, int k, int[][] invocations) {
 3        Map<Integer,List<Integer>> map=new HashMap<>();
@@ -11,46 +11,31 @@
 10            map.get(u).add(v);
 11        }
 12        boolean vis[]=new boolean[n];
-13        Arrays.fill(vis,true);
-14        Stack<Integer> st=new Stack<>();
-15        st.push(k);
-16        Set<Integer> set=new HashSet<>();
-17        while(!st.isEmpty()){
-18            int r=st.pop();
-19            if(set.contains(r)) continue;
-20            set.add(r);
-21            vis[r]=false;
-22            for(int nbr:map.get(r)){
-23                if(!set.contains(nbr)) st.push(nbr);
-24            }
-25        }
-26        Set<Integer> nset=new HashSet<>();
-27        for(int i:map.keySet()){
-28            if(!set.contains(i)){
-29                st=new Stack<>();
-30                st.push(i);
-31                while(!st.isEmpty()){
-32                    int r=st.pop();
-33                    if(nset.contains(r)) continue;
-34                    nset.add(r);
-35                    vis[r]=true;
-36                    if(set.contains(r)){
-37                        List<Integer> ans=new ArrayList<>();
-38                        for(int v=0;v<n;v++){
-39                            ans.add(v);
-40                        }
-41                        return ans;
-42                    }
-43                    for(int nbr:map.get(r)){
-44                        if(!nset.contains(nbr)) st.push(nbr);
-45                    }
-46                }
-47            }
-48        }
-49        List<Integer> ans=new ArrayList<>();
-50        for(int i=0;i<n;i++){
-51            if(vis[i]) ans.add(i);
-52        }
-53        return ans;
-54    }
-55}
+13        Stack<Integer> st=new Stack<>();
+14        st.push(k);
+15        while(!st.isEmpty()){
+16            int r=st.pop();
+17            if(vis[r]) continue;
+18            vis[r]=true;
+19            for(int nbr:map.get(r)){
+20                if(!vis[nbr]) st.push(nbr);
+21            }
+22        }
+23        for(int e[]:invocations){
+24            int u=e[0];
+25            int v=e[1];
+26            if(!vis[u] && vis[v]){
+27                List<Integer> ans=new ArrayList<>();
+28                for(int i=0;i<n;i++){
+29                    ans.add(i);
+30                }
+31                return ans;
+32            }
+33        }
+34        List<Integer> ans=new ArrayList<>();
+35        for(int i=0;i<n;i++){
+36            if(!vis[i]) ans.add(i);
+37        }
+38        return ans;
+39    }
+40}
