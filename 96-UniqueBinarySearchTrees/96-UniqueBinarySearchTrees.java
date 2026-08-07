e29e94d1d6aa2b049ecadb76c9328094dde8784a
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-08-07 22:45:11 +0530

    File updated at 8/7/2026, 10:45:09 PM

--- 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
+++ 96-UniqueBinarySearchTrees/96-UniqueBinarySearchTrees.java
@@ -1,9 +1,16 @@
-// Last updated: 8/7/2026, 10:42:47 PM
+// Last updated: 8/7/2026, 10:45:09 PM
 1class Solution {
-2    public int numTrees(int n) {
-3       if(n<=1) return 1;
-4       int ans=0;
-5       for(int i=1;i<=n;i++) ans+=(numTrees(i-1)*numTrees(n-i));
-6       return ans;
+2    int[] dp;
+3    public int numTrees(int n) {
+4       dp=new int[n+1];
+5       Arrays.fill(dp,-1);
+6       return find(n);
 7    }
-8}
+8    public int find(int n){
+9        if(n<=1) return 1;
+10        if(dp[n]!=-1) return dp[n];
+11       int ans=0;
+12       for(int i=1;i<=n;i++) ans+=(find(i-1)*find(n-i));
+13       return dp[n]=ans;
+14    }
+15}
