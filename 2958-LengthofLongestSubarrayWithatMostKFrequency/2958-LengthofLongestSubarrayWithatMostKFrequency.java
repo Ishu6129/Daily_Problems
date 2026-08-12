f584136474e15e746bf438941e5c69047e6cdc83
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-08-12 10:19:25 +0530

    File updated at 8/12/2026, 10:19:24 AM

--- 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.java
+++ 2958-LengthofLongestSubarrayWithatMostKFrequency/2958-LengthofLongestSubarrayWithatMostKFrequency.java
@@ -1,4 +1,4 @@
-// Last updated: 8/12/2026, 10:17:20 AM
+// Last updated: 8/12/2026, 10:19:24 AM
 1class Solution {
 2    public int maxSubarrayLength(int[] nums, int k) {
 3        Map<Integer,Integer> map=new HashMap<>();
@@ -7,17 +7,14 @@
 6        int j=0;
 7        int n=nums.length;
 8        while(i<n && j<n){
-9            map.putIfAbsent(nums[j],0);
-10            map.put(nums[j],map.get(nums[j])+1);
-11            if(map.containsKey(nums[j])){
-12                while(map.get(nums[j])>k){
-13                    map.put(nums[i],map.get(nums[i])-1);
-14                    i++;
-15                }
-16            }
-17            ans=Math.max(ans,j-i+1);
-18            j++;
-19        }
-20        return ans;
-21    }
-22}
+9            map.put(nums[j], map.getOrDefault(nums[j],0)+1);
+10            while(map.get(nums[j])>k){
+11                map.put(nums[i],map.get(nums[i])-1);
+12                i++;
+13            }
+14            ans=Math.max(ans,j-i+1);
+15            j++;
+16        }
+17        return ans;
+18    }
+19}
