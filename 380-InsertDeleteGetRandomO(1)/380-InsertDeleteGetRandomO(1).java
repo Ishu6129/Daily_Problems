88b8606e9e57c16b0f22fe9c5731eebba31f8e07
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-08-19 00:23:27 +0530

    File updated at 8/19/2026, 12:23:23 AM

--- 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).java
+++ 380-InsertDeleteGetRandomO(1)/380-InsertDeleteGetRandomO(1).java
@@ -1,33 +1,46 @@
-// Last updated: 8/19/2026, 12:01:44 AM
-1class RandomizedSet {
-2    Set<Integer> set;
-3    public RandomizedSet() {
-4        set=new HashSet<>();
-5    }
-6    
-7    public boolean insert(int val) {
-8        return set.add(val);
+// Last updated: 8/19/2026, 12:23:23 AM
+1class RandomizedCollection {
+2    Map<Integer,Integer> map;
+3    List<Integer> ls;
+4    int total;
+5    public RandomizedCollection() {
+6        map=new HashMap<>();
+7        ls=new ArrayList<>();
+8        total=0;
 9    }
 10    
-11    public boolean remove(int val) {
-12        return set.remove(val);
-13    }
-14    
-15    public int getRandom() {
-16        int idx=(int)(Math.random()*set.size());
-17        for(int i:set){
-18            if(idx--==0){
-19                return i;
-20            }
-21        }
-22        return -1;
-23    }
-24}
-25
-26/**
-27 * Your RandomizedSet object will be instantiated and called as such:
-28 * RandomizedSet obj = new RandomizedSet();
-29 * boolean param_1 = obj.insert(val);
-30 * boolean param_2 = obj.remove(val);
-31 * int param_3 = obj.getRandom();
-32 */
+11    public boolean insert(int val) {
+12        ls.add(val);
+13        total++;
+14        if(!map.containsKey(val)){
+15            map.put(val,1);
+16            return true;
+17        }
+18        map.put(val,map.get(val)+1);
+19        return false;
+20    }
+21    
+22    public boolean remove(int val) {
+23        if(map.containsKey(val)){
+24            map.put(val,map.get(val)-1);
+25            if(map.get(val)==0) map.remove(val);
+26            ls.remove(Integer.valueOf(val));
+27            total--;
+28            return true;
+29        }
+30        return false;
+31    }
+32    
+33    public int getRandom() {
+34        int idx=(int)(Math.random()*total);
+35        return ls.get(idx);
+36    }
+37}
+38
+39/**
+40 * Your RandomizedCollection object will be instantiated and called as such:
+41 * RandomizedCollection obj = new RandomizedCollection();
+42 * boolean param_1 = obj.insert(val);
+43 * boolean param_2 = obj.remove(val);
+44 * int param_3 = obj.getRandom();
+45 */
