28280282645b05d931506d5faab4569adaa0943c
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-08-30 20:31:15 +0530

    File updated at 8/30/2026, 8:31:14 PM

--- 1537-GettheMaximumScore/1537-GettheMaximumScore.java
+++ 1537-GettheMaximumScore/1537-GettheMaximumScore.java
@@ -1,30 +1,26 @@
-// Last updated: 23/03/2026, 17:49:46
+// Last updated: 8/30/2026, 8:31:14 PM
 1class Solution {
-2    int mod=(int) 1e9+7;
-3    public int maxSum(int[] nums1, int[] nums2) {
-4        long sum1=0;
-5        long sum2=0;
-6        long ans=0;
-7        int m=nums1.length;
-8        int n=nums2.length;
-9        int i=0;
-10        int j=0;
-11        while(i<m && j<n){
-12            if(nums1[i]<nums2[j]) sum1+=nums1[i++];
-13            else if(nums2[j]<nums1[i]) sum2+=nums2[j++];
-14            else{
-15                ans+=Math.max(sum1,sum2)+nums1[i];
-16                sum1=0;
-17                sum2=0;
-18                i++;
-19                j++;
-20            }
-21        }
-22        while(i<m){
-23            sum1+=nums1[i++];
-24        }
-25        while(j<n) sum2+=nums2[j++];
-26        ans+=Math.max(sum1,sum2);
-27        return  (int) (ans%mod);
-28    }
-29}
+2    public int maxSum(int[] nums1, int[] nums2) {
+3        int n=nums1.length;
+4        int m=nums2.length;
+5        int i=0;
+6        int j=0;
+7        long sm1=0;
+8        long sm2=0;
+9        long ans=0;
+10        while(i<n && j<m){
+11            if(nums1[i]<nums2[j]) sm1+=nums1[i++];
+12            else if(nums2[j]<nums1[i]) sm2+=nums2[j++];
+13            else{
+14                ans+=Math.max(sm1,sm2)+nums1[i];
+15                sm1=0;
+16                sm2=0;
+17                i++;j++;
+18            }
+19        }
+20        while(i<n) sm1+=nums1[i++];
+21        while(j<m) sm2+=nums2[j++];
+22        ans+=Math.max(sm1,sm2);
+23        return  (int) (ans%((int)(1e9+7)));
+24    }
+25}
