652b204da5e136857cf1668ff647255e670aca30
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-09-01 18:35:57 +0530

    File updated at 9/1/2026, 6:35:55 PM

--- 25-ReverseNodesink-Group/25-ReverseNodesink-Group.java
+++ 25-ReverseNodesink-Group/25-ReverseNodesink-Group.java
@@ -1,52 +1,52 @@
-// Last updated: 31/10/2025, 15:45:05
-/**
- * Definition for singly-linked list.
- * public class ListNode {
- *     int val;
- *     ListNode next;
- *     ListNode() {}
- *     ListNode(int val) { this.val = val; }
- *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
- * }
- */
-class Solution {
-    public ListNode reverseKGroup(ListNode head, int k) {
-        int size=0;
-        ListNode dummy=head;
-        while(dummy!=null){
-            size++;
-            dummy=dummy.next;
-        }
-        dummy=head;
-        ListNode pre=head;
-        ListNode ans=new ListNode(-1);
-        ListNode curr=ans;
-        for(int i=1;i<=size;i++){
-            if(i%k==0){
-                ListNode temp=dummy.next;
-                dummy.next=null;
-                curr.next=reverse(pre);
-                pre=temp;
-                for(int j=0;j<k;j++){
-                    curr=curr.next;
-                }
-                dummy=temp;
-            }
-            else{
-                dummy=dummy.next;
-            }
-        }
-        if (pre != null) curr.next = pre;
-        return ans.next;
-    }
-    public ListNode reverse(ListNode curr){
-        ListNode prev=null;
-        while(curr!=null){
-            ListNode temp=curr.next;
-            curr.next=prev;
-            prev=curr;
-            curr=temp;
-        }
-        return prev;
-    }
-}
+// Last updated: 9/1/2026, 6:35:55 PM
+1/**
+2 * Definition for singly-linked list.
+3 * public class ListNode {
+4 *     int val;
+5 *     ListNode next;
+6 *     ListNode() {}
+7 *     ListNode(int val) { this.val = val; }
+8 *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+9 * }
+10 */
+11class Solution {
+12    public ListNode reverseKGroup(ListNode head, int k) {
+13        int size=0;
+14        ListNode dummy=head;
+15        while(dummy!=null){
+16            size++;
+17            dummy=dummy.next;
+18        }
+19        dummy=head;
+20        ListNode pre=head;
+21        ListNode ans=new ListNode(-1);
+22        ListNode curr=ans;
+23        for(int i=1;i<=size;i++){
+24            if(i%k==0){
+25                ListNode temp=dummy.next;
+26                dummy.next=null;
+27                curr.next=reverse(pre);
+28                pre=temp;
+29                for(int j=0;j<k;j++){
+30                    curr=curr.next;
+31                }
+32                dummy=temp;
+33            }
+34            else{
+35                dummy=dummy.next;
+36            }
+37        }
+38        if (pre != null) curr.next = pre;
+39        return ans.next;
+40    }
+41    public ListNode reverse(ListNode curr){
+42        ListNode prev=null;
+43        while(curr!=null){
+44            ListNode temp=curr.next;
+45            curr.next=prev;
+46            prev=curr;
+47            curr=temp;
+48        }
+49        return prev;
+50    }
+51}
