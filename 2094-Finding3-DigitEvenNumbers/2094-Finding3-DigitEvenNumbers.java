7602a9e0ca28b9e42ed4b73130ddc05139d05c7f
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-09-11 09:34:51 +0530

    File updated at 9/11/2026, 9:34:40 AM

--- 2094-Finding3-DigitEvenNumbers/2094-Finding3-DigitEvenNumbers.java
+++ 2094-Finding3-DigitEvenNumbers/2094-Finding3-DigitEvenNumbers.java
@@ -1,19 +1,15 @@
-// Last updated: 9/11/2026, 9:33:17 AM
+// Last updated: 9/11/2026, 9:34:40 AM
 1class Solution {
 2    public int[] findEvenNumbers(int[] digits) {
-3        int n=digits.length;
-4        Set<Integer> set=new HashSet<>();
-5        for(int i=0;i<n;i++){
-6            for(int j=0;j<n;j++){
-7                for(int k=0;k<n;k++){
-8                    if(i==j || j==k || k==i) continue;
-9                    int num=((digits[i]*10+digits[j])*10+digits[k]);
-10                    if(num>99 && !set.contains(num) && (num&1)!=1){
-11                        set.add(num);
-12                    }
-13                }
-14            }
-15        }
-16        return set.stream().mapToInt(Integer::intValue).sorted().toArray();
-17    }
-18}
+3        int n = digits.length;
+4        Set<Integer> set = IntStream.range(0, n).boxed()
+5            .flatMap(i -> IntStream.range(0, n).boxed()
+6                .filter(j -> j != i)
+7                .flatMap(j -> IntStream.range(0, n).boxed()
+8                    .filter(k -> k != i && k != j)
+9                    .map(k -> (digits[i] * 10 + digits[j]) * 10 + digits[k])))
+10            .filter(num -> num > 99 && (num & 1) == 0)
+11            .collect(Collectors.toSet()); 
+12        return set.stream().mapToInt(Integer::intValue).sorted().toArray();
+13    }
+14}
