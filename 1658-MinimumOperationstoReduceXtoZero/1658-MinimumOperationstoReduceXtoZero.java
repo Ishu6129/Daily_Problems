915230005ce62d03f65772246c70aa60a6b369aa
Ishu Agrawal <165049137+Ishu6129@users.noreply.github.com>
2026-09-23 12:48:22 +0530

    Create 1658-MinimumOperationstoReduceXtoZero

---
+++ 1658-MinimumOperationstoReduceXtoZero/1658-MinimumOperationstoReduceXtoZero.java
@@ -0,0 +1,25 @@
+// Last updated: 9/23/2026, 12:48:03 PM
+1class Solution {
+2    public int minOperations(int[] nums, int x) {
+3        int sum=0;
+4        for(int i:nums) sum+=i;
+5        int target = sum-x;
+6        if(target<0) return -1;
+7        int n=nums.length;
+8        if(target==0) return n;
+9        int mx=-1;
+10        int curr=0;
+11        int l=0;
+12        for(int i=0;i<n;i++){
+13            curr+=nums[i];
+14            while(curr>target && l<=i){
+15                curr-=nums[l];
+16                l++;
+17            }
+18            if(curr==target){
+19                mx=Math.max(mx,i-l+1);
+20            }
+21        }
+22        return mx==-1?-1:n-mx;
+23    }
+24}
